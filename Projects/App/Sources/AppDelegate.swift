8ed798e1784b1208e1f88ab4e805d83fbe74bf31
hsj._.06 <gtw030488@gmail.com>
2023-09-26 23:19:59 +0900

    feat :: Swiftlint init

--- Projects/App/Sources/AppDelegate.swift
+++ Projects/App/Sources/AppDelegate.swift
@@ -5,7 +5,7 @@ import Presentation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     static var container: Container {
         let container = Container()
         container.register(MainViewModel.self) { _ in
@@ -16,26 +16,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return container
     }
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) { }
 }
-
--- Projects/Presentation/Sources/Main/MainViewModel.swift
+++ Projects/Presentation/Sources/Main/MainViewModel.swift
@@ -6,19 +6,20 @@ import Core
 
 public class MainViewModel: BaseViewModel, Stepper {
     public var steps = PublishRelay<Step>()
-    
+
     private let disposeBag = DisposeBag()
+
     public struct Input {
-        let ButtonDidTap: Signal<Void>
+        let buttonDidTap: Signal<Void>
     }
-    
+
     public struct Output {
         let result: PublishRelay<Bool>
     }
 
     public func transform(_ input: Input) -> Output {
         let result = PublishRelay<Bool>()
-        input.ButtonDidTap.asObservable()
+        input.buttonDidTap.asObservable()
             .map { MainStep.loginIsRequired }
             .bind(to: steps)
             .disposed(by: disposeBag)
