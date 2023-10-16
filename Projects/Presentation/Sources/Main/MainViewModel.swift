870a985109a24c25f2ed425dc326252192a9521d
HongSJae <gtw030488@gmail.com>
2023-10-16 22:52:57 +0900

    feat :: Main 버튼 클릭시 인증 보냄

--- Projects/Presentation/Sources/Main/MainViewModel.swift
+++ Projects/Presentation/Sources/Main/MainViewModel.swift
@@ -3,11 +3,18 @@ import RxSwift
 import RxCocoa
 import RxFlow
 import Core
+import Domain
 
 public class MainViewModel: BaseViewModel, Stepper {
     public var steps = PublishRelay<Step>()
 
     private let disposeBag = DisposeBag()
+
+    private let usecase: SendAuthCodeUseCase
+
+    init(usecase: SendAuthCodeUseCase) {
+        self.usecase = usecase
+    }
 
     public struct Input {
         let buttonDidTap: Signal<Void>
@@ -20,10 +27,13 @@ public class MainViewModel: BaseViewModel, Stepper {
     public func transform(_ input: Input) -> Output {
         let result = PublishRelay<Bool>()
         input.buttonDidTap.asObservable()
-            .map { MainStep.loginIsRequired }
+            .flatMap { [self] in
+                usecase.execute(req: .init(email: "gtw030488@gmail.com", authCodeType: .signup))
+                    .andThen(Single.just(MainStep.loginIsRequired))
+                    .catch { _ in .just(MainStep.loginIsRequired) }
+            }
             .bind(to: steps)
             .disposed(by: disposeBag)
         return Output(result: result)
     }
-    public init() { }
 }
