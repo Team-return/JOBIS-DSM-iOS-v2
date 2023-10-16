7110017144fc50e1c4d29d38c357ed92d44c9ea7
HongSJae <gtw030488@gmail.com>
2023-10-16 23:05:09 +0900

    feat :: KeychainAssembly

---
+++ Projects/Core/Sources/JwtStore/KeychainAssembly.swift
@@ -0,0 +1,11 @@
+import Foundation
+import Swinject
+public final class KeychainAssembly: Assembly {
+    public init() {}
+
+    public func assemble(container: Container) {
+        container.register(Keychain.self) { _ in
+            KeychainImpl()
+        }
+    }
+}
