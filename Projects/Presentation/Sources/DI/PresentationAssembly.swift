fa1df6736e8af899e81966247a47cc3941492f33
hsj._.06 <gtw030488@gmail.com>
2023-10-17 16:54:17 +0900

    fix :: Main 에 로그인, 리프레쉬 테스트 추가

--- Projects/Presentation/Sources/DI/PresentationAssembly.swift
+++ Projects/Presentation/Sources/DI/PresentationAssembly.swift
@@ -8,7 +8,10 @@ public final class PresentationAssembly: Assembly {
 
     public func assemble(container: Container) {
         container.register(MainViewModel.self) { resolver in
-            MainViewModel(usecase: resolver.resolve(SendAuthCodeUseCase.self)!)
+            MainViewModel(
+                signinUseCase: resolver.resolve(SigninUseCase.self)!,
+                reissueTokenUseCase: resolver.resolve(ReissueTokenUaseCase.self)!
+            )
         }
         container.register(MainViewController.self) { resolver in
             MainViewController(resolver.resolve(MainViewModel.self)!)
