370f7ca3718f49359d48e81a5d91da04744ea7ae
juyeong525 <juyeongno1@naver.com>
2023-10-17 19:06:49 +0900

    feat :: Student Remote

---
+++ Projects/Data/Sources/DataSource/Remote/StudentsRemote.swift
@@ -0,0 +1,51 @@
+import Domain
+import RxCocoa
+import RxSwift
+
+public protocol StudentsRemote {
+    func signup(req: SignupRequestQuery) -> Completable
+    func renewalPassword(req: RenewalPasswordRequestQuery) -> Completable
+    func studentExists(gcn: Int, name: String) -> Completable
+    func fetchStudentInfo() -> Single<StudentInfoEntity>
+    func changePassword(req: ChangePasswordRequestQuery) -> Completable
+    func compareCurrentPasssword(password: String) -> Completable
+    func changeProfileImage(url: String) -> Completable
+}
+
+final class StudentsRemoteImpl: BaseRemote<StudentsAPI>, StudentsRemote {
+    func signup(req: SignupRequestQuery) -> Completable {
+        request(.signup(req))
+            .asCompletable()
+    }
+
+    func renewalPassword(req: RenewalPasswordRequestQuery) -> Completable {
+        request(.renewalPassword(req))
+            .asCompletable()
+    }
+
+    func studentExists(gcn: Int, name: String) -> Completable {
+        request(.studentExists(gcn: gcn, name: name))
+            .asCompletable()
+    }
+
+    func fetchStudentInfo() -> Single<StudentInfoEntity> {
+        request(.fetchStudentInfo)
+            .map(StudentInfoResponseDTO.self)
+            .map { $0.toDomain() }
+    }
+
+    func changePassword(req: ChangePasswordRequestQuery) -> Completable {
+        request(.changePassword(req))
+            .asCompletable()
+    }
+
+    func compareCurrentPasssword(password: String) -> Completable {
+        request(.compareCurrentPasssword(password: password))
+            .asCompletable()
+    }
+
+    func changeProfileImage(url: String) -> Completable {
+        request(.changeProfileImage(url: url))
+            .asCompletable()
+    }
+}
