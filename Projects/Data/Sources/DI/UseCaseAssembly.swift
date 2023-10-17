b7be6f96c7d6de4eccb004c7b710291aad8bc762
hsj._.06 <gtw030488@gmail.com>
2023-10-17 16:53:18 +0900

    feat :: SigninUseCase DI

--- Projects/Data/Sources/DI/UseCaseAssembly.swift
+++ Projects/Data/Sources/DI/UseCaseAssembly.swift
@@ -6,21 +6,27 @@ public final class UseCaseAssembly: Assembly {
     public init() {}
 
     public func assemble(container: Container) {
+        //Auth
         container.register(SendAuthCodeUseCase.self) { resolver in
             SendAuthCodeUseCase(
                 authRepository: resolver.resolve(AuthRepository.self)!
             )
         }
-
         container.register(VerifyAuthCodeUseCase.self) { resolver in
             VerifyAuthCodeUseCase(
                 authRepository: resolver.resolve(AuthRepository.self)!
             )
         }
-
         container.register(ReissueTokenUaseCase.self) { resolver in
             ReissueTokenUaseCase(
                 authRepository: resolver.resolve(AuthRepository.self)!
+            )
+        }
+
+        // Users
+        container.register(SigninUseCase.self) { resolver in
+            SigninUseCase(
+                usersRepository: resolver.resolve(UsersRepository.self)!
             )
         }
     }
