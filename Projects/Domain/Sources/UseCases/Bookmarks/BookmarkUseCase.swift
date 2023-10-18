8dd7e62b87e9dc81229d103c8b2268550db3634f
juyeong525 <juyeongno1@naver.com>
2023-10-18 13:46:32 +0900

    feat :: Bookmarks UseCases

---
+++ Projects/Domain/Sources/UseCases/Bookmarks/BookmarkUseCase.swift
@@ -0,0 +1,14 @@
+import RxSwift
+import RxCocoa
+
+public struct BookmarkUseCase {
+    private let bookmarksRepository: any BookmarksRepository
+
+    public init(bookmarksRepository: any BookmarksRepository) {
+        self.bookmarksRepository = bookmarksRepository
+    }
+
+    public func execute(id: Int) -> Completable {
+        bookmarksRepository.bookmark(id: id)
+    }
+}
