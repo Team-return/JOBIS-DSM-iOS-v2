3bb2334850223a84471b49f53cee2a085e4008a2
hsj._.06 <gtw030488@gmail.com>
2023-10-18 11:22:19 +0900

    feat :: Codes Remote DI

--- Projects/Data/Sources/DI/DataSourceAssembly.swift
+++ Projects/Data/Sources/DI/DataSourceAssembly.swift
@@ -30,5 +30,9 @@ public final class DataSourceAssembly: Assembly {
         container.register(BugsRemote.self) { resolver in
             BugsRemoteImpl(keychainLocal: self.keychain(resolver))
         }
+
+        container.register(CodesRemote.self) { resolver in
+            CodesRemoteImpl(keychainLocal: self.keychain(resolver))
+        }
     }
 }
