7abe0dbf37761ecacb76677333d478c5c22d77bb
juyeong525 <juyeongno1@naver.com>
2023-10-18 12:03:48 +0900

    feat :: Applications UseCases

---
+++ Projects/Domain/Sources/UseCases/Applications/ApplyCompanyUseCase.swift
@@ -0,0 +1,13 @@
+import RxCocoa
+import RxSwift
+
+public struct ApplyCompanyUseCase {
+    private let applicationRepository: any ApplicationsRepository
+
+    public init(applicationRepository: any ApplicationsRepository) {
+        self.applicationRepository = applicationRepository
+    }
+    func execute(id: String, req: ApplyCompanyRequestQuery) -> Completable {
+        applicationRepository.applyCompany(id: id, req: req)
+    }
+}
