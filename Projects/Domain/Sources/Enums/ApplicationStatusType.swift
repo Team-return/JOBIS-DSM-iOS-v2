e5ec0d61e29654167f196d512c89e5e31b331e70
juyeong525 <juyeongno1@naver.com>
2023-10-18 12:04:51 +0900

    feat :: ApplicationStatusType, AttachmentType

---
+++ Projects/Domain/Sources/Enums/ApplicationStatusType.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+public enum ApplicationStatusType: String, Codable {
+    case requested = "REQUESTED"
+    case approved = "APPROVED"
+    case failed = "FAILED"
+    case pass = "PASS"
+    case rejected = "REJECTED"
+    case fieldTrain = "FIELD_TRAIN"
+
+    public func localizedString() -> String {
+        switch self {
+        case .requested:
+            return "요청됨"
+
+        case .approved:
+            return "승인됨"
+
+        case .failed:
+            return "탈락"
+
+        case .pass:
+            return "합격"
+
+        case .rejected:
+            return "반려"
+
+        case .fieldTrain:
+            return "현장 실습"
+        }
+    }
+}
