d4bb746925d270ec8de43a88959122de699439c0
hsj._.06 <gtw030488@gmail.com>
2023-11-05 19:50:53 +0900

    feat :: Area Model Public화 및 우대사항 추가

--- Projects/Data/Sources/DTO/Recruitments/AreaResponseDTO.swift
+++ Projects/Data/Sources/DTO/Recruitments/AreaResponseDTO.swift
@@ -1,27 +1,46 @@
 import Foundation
 import Domain
 
-struct AreaResponseDTO: Decodable {
-    let id: Int
-    let job: [String]
-    let tech: [String]
-    let hiring: Int
-    let majorTask: String
+public struct AreaResponseDTO: Decodable {
+    public let id: Int
+    public let job: [String]
+    public let tech: [String]
+    public let hiring: Int
+    public let majorTask: String
+    public let preferentialTreatment: String?
+
+    public init(
+        id: Int,
+        job: [String],
+        tech: [String],
+        hiring: Int,
+        majorTask: String,
+        preferentialTreatment: String?
+    ) {
+        self.id = id
+        self.job = job
+        self.tech = tech
+        self.hiring = hiring
+        self.majorTask = majorTask
+        self.preferentialTreatment = preferentialTreatment
+    }
 
     enum CodingKeys: String, CodingKey {
         case id, job, tech, hiring
         case majorTask = "major_task"
+        case preferentialTreatment = "preferential_treatment"
     }
 }
 
-extension AreaResponseDTO {
+public extension AreaResponseDTO {
     func toDomain() -> AreaEntity {
         AreaEntity(
             id: String(id),
             job: job.joined(separator: ", "),
             tech: tech,
             hiring: hiring,
-            majorTask: majorTask
+            majorTask: majorTask,
+            preferentialTreatment: preferentialTreatment
         )
     }
 }
