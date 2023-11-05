e7101e5d2045c4642cea6c9badba83d3e768997b
hsj._.06 <gtw030488@gmail.com>
2023-11-05 19:50:53 +0900

    feat :: Area Model Public화 및 우대사항 추가

--- Projects/Domain/Sources/Entities/Recruitments/AreaEntity.swift
+++ Projects/Domain/Sources/Entities/Recruitments/AreaEntity.swift
@@ -6,18 +6,21 @@ public struct AreaEntity: Equatable, Hashable {
     public let tech: [String]
     public let hiring: Int
     public let majorTask: String
+    public let preferentialTreatment: String?
 
     public init(
         id: String,
         job: String,
         tech: [String],
         hiring: Int,
-        majorTask: String
+        majorTask: String,
+        preferentialTreatment: String?
     ) {
         self.id = id
         self.job = job
         self.tech = tech
         self.hiring = hiring
         self.majorTask = majorTask
+        self.preferentialTreatment = preferentialTreatment
     }
 }
