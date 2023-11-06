c74c7dafc7f50721a8ea3d80e9e2dd10dcdda309
HongSJae <gtw030488@gmail.com>
2023-11-06 15:28:57 +0900

    feat :: Colors Structure 추가

---
+++ Projects/Modules/DesignSystem/Sources/Color/JOBISMainColor.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+public extension UIColor {
+    struct Main { }
+}
+
+public extension UIColor.Main {
+    static let blue1: UIColor = DesignSystemAsset.Main.blue1.color
+    static let blue2: UIColor = DesignSystemAsset.Main.blue2.color
+    static let blue3: UIColor = DesignSystemAsset.Main.blue3.color
+}
---
+++ Projects/Modules/DesignSystem/Sources/Color/JOBISSubScaleColor.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+public extension UIColor {
+    struct GrayScale { }
+}
+
+public extension UIColor.GrayScale {
+    static let gray10: UIColor = DesignSystemAsset.GrayScale.gray10.color
+    static let gray20: UIColor = DesignSystemAsset.GrayScale.gray20.color
+    static let gray30: UIColor = DesignSystemAsset.GrayScale.gray30.color
+    static let gray40: UIColor = DesignSystemAsset.GrayScale.gray40.color
+    static let gray50: UIColor = DesignSystemAsset.GrayScale.gray50.color
+    static let gray60: UIColor = DesignSystemAsset.GrayScale.gray60.color
+    static let gray70: UIColor = DesignSystemAsset.GrayScale.gray70.color
+    static let gray80: UIColor = DesignSystemAsset.GrayScale.gray80.color
+    static let gray90: UIColor = DesignSystemAsset.GrayScale.gray90.color
+}
