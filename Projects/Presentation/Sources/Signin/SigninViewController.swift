a670a81589579fc5d0f863290df5d51e7c9ec405
HongSJae <gtw030488@gmail.com>
2023-11-07 15:00:35 +0900

    feat :: Signin View init

---
+++ Projects/Presentation/Sources/Signin/SigninViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+import Core
+import DesignSystem
+
+public class SigninViewController: BaseViewController<SigninViewModel> {
+    private let label = UILabel().then {
+        $0.setJobisText("로그인", font: .body, color: .black)
+    }
+    public override func addView() {
+        self.view.addSubview(label)
+    }
+    public override func layout() {
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    public override func attribute() {
+        self.view.backgroundColor = .GrayScale.gray10
+    }
+}
