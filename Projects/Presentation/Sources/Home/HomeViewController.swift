6f2dbf0802eb2669f58579eaeea5a2b169db6c81
hsj._.06 <gtw030488@gmail.com>
2023-11-07 19:30:35 +0900

    feat :: Home view init

---
+++ Projects/Presentation/Sources/Home/HomeViewController.swift
@@ -0,0 +1,71 @@
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+import Core
+import DesignSystem
+
+public class HomeViewController: BaseViewController<HomeViewModel> {
+    let loading = UIActivityIndicatorView().then {
+        $0.style = .medium
+    }
+    let label = UILabel().then {
+        $0.setJobisText("개인정보를 입력해주세요", font: .pageTitle)
+    }
+
+    let signinButton = JobisButton(style: .main).then {
+        $0.setText("로그인")
+    }
+
+    let tokenButton = JobisButton(style: .sub).then {
+        $0.setText("토큰 재발급")
+        $0.isEnabled = false
+    }
+
+    public override func layout() {
+        signinButton.snp.makeConstraints {
+            $0.bottom.equalTo(tokenButton.snp.top).inset(-20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        tokenButton.snp.makeConstraints {
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        loading.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(label.snp.top).offset(-30)
+        }
+    }
+
+    public override func addView() {
+        [
+            signinButton,
+            tokenButton,
+            label,
+            loading
+        ].forEach { self.view.addSubview($0) }
+    }
+
+    public override func bind() {
+        let input = HomeViewModel.Input(
+            signinButtonDidTap: signinButton.rx.tap.asSignal(),
+            reissueButtonDidTap: tokenButton.rx.tap.asSignal()
+        )
+        let output = viewModel.transform(input)
+        output.string
+            .bind {
+                self.label.text = $0
+                self.tokenButton.isEnabled = true
+            }
+            .disposed(by: disposeBag)
+        output.isLoading
+            .subscribe(onNext: { [self] bool in
+                bool ? loading.startAnimating() : loading.stopAnimating()
+            })
+            .disposed(by: disposeBag)
+    }
+}
---
+++ Projects/Presentation/Sources/Home/HomeViewModel.swift
@@ -0,0 +1,80 @@
+import UIKit
+import RxSwift
+import RxCocoa
+import RxFlow
+import Core
+import Domain
+
+public class HomeViewModel: BaseViewModel, Stepper {
+    public var steps = PublishRelay<Step>()
+
+    private let disposeBag = DisposeBag()
+
+    private let signinUseCase: SigninUseCase
+    private let reissueTokenUseCase: ReissueTokenUaseCase
+
+    init(signinUseCase: SigninUseCase, reissueTokenUseCase: ReissueTokenUaseCase) {
+        self.signinUseCase = signinUseCase
+        self.reissueTokenUseCase = reissueTokenUseCase
+    }
+
+    public struct Input {
+        let signinButtonDidTap: Signal<Void>
+        let reissueButtonDidTap: Signal<Void>
+    }
+
+    public struct Output {
+        let string: PublishRelay<String>
+        let result: PublishRelay<Bool>
+        let isLoading: BehaviorRelay<Bool>
+    }
+
+    public func transform(_ input: Input) -> Output {
+        let string = PublishRelay<String>()
+        let result = PublishRelay<Bool>()
+        let isLoading: BehaviorRelay<Bool> = .init(value: false)
+        input.signinButtonDidTap.asObservable()
+            .flatMap { [self] in
+                isLoading.accept(true)
+                return signinUseCase.execute(
+                    req: .init(
+                        accountID: "test@dsm.hs.kr",
+                        password: "student"
+                    )
+                )
+                .do(
+                    onSuccess: {
+                    string.accept($0.rawValue)
+                    print($0)
+                    isLoading.accept(false)
+                    }, onError: { _ in
+                        isLoading.accept(false)
+                    }
+                )
+                .asCompletable()
+//                .andThen(Single.just(MainStep.loginIsRequired))
+            }
+            .subscribe(onCompleted: .none)
+            .disposed(by: disposeBag)
+
+        input.reissueButtonDidTap.asObservable()
+            .flatMap { [self] in
+                isLoading.accept(true)
+                return reissueTokenUseCase.execute()
+                    .do(
+                        onSuccess: {
+                            string.accept($0.rawValue)
+                            print($0)
+                            isLoading.accept(false)
+                        }, onError: { _ in
+                            isLoading.accept(false)
+                        }
+                    )
+                    .asCompletable()
+//                .andThen(Single.just(MainStep.loginIsRequired))
+            }
+            .subscribe(onCompleted: .none)
+            .disposed(by: disposeBag)
+        return Output(string: string, result: result, isLoading: isLoading)
+    }
+}
