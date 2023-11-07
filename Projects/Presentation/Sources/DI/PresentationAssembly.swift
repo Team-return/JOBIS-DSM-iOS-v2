6abafae9c18d1ff1025f33be6654de9c607a7535
HongSJae <gtw030488@gmail.com>
2023-11-07 15:04:17 +0900

    feat :: Onboarding View publishing
    추후 Lottie 수정 및 애니메이션 수정 필요

--- Projects/Presentation/Sources/DI/PresentationAssembly.swift
+++ Projects/Presentation/Sources/DI/PresentationAssembly.swift
@@ -16,6 +16,14 @@ public final class PresentationAssembly: Assembly {
         container.register(MainViewController.self) { resolver in
             MainViewController(resolver.resolve(MainViewModel.self)!)
         }
+
+        container.register(OnboardingViewModel.self) { resolver in
+            OnboardingViewModel()
+        }
+        container.register(OnboardingViewController.self) { resolver in
+            OnboardingViewController(resolver.resolve(OnboardingViewModel.self)!)
+        }
+
         container.register(SignupViewModel.self) { resolver in
             SignupViewModel()
         }
