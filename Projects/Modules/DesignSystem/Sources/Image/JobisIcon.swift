201cf63ecd2413b23102dd7087ef9194aa9353d4
juyeong525 <juyeongno1@naver.com>
2023-11-08 21:32:13 +0900

    feat :: JobisIcon

--- Projects/Modules/DesignSystem/Sources/Image/JobisIcon.swift
+++ Projects/Modules/DesignSystem/Sources/Image/JobisIcon.swift
@@ -9,6 +9,11 @@ extension UIImage {
 
 enum JobisIcon {
     case arrowRight
+    case erorr
+    case info
+    case success
+    case eyeOn
+    case eyeOff
 
     func uiImage() -> UIImage {
         let dsIcon = DesignSystemAsset.Icons.self
@@ -16,6 +21,16 @@ enum JobisIcon {
         switch self {
         case .arrowRight:
             return dsIcon.arrowRight.image
+        case .erorr:
+            return dsIcon.error.image
+        case .info:
+            return dsIcon.info.image
+        case .success:
+            return dsIcon.success.image
+        case .eyeOn:
+            return dsIcon.eyeOn.image
+        case .eyeOff:
+            return dsIcon.eyeOff.image
         }
     }
 }
