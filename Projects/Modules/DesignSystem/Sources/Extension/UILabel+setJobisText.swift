3bad21a3ba48d3fc2797b255a5b41ddcda75a9ca
juyeong525 <juyeongno1@naver.com>
2023-11-08 21:27:36 +0900

    feat :: text baselineOffset 수정

--- Projects/Modules/DesignSystem/Sources/Extension/UILabel+setJobisText.swift
+++ Projects/Modules/DesignSystem/Sources/Extension/UILabel+setJobisText.swift
@@ -2,7 +2,7 @@ import UIKit
 
 public extension UILabel {
     func setJobisText(_ text: String, font: JobisFontStyle, color: UIColor? = nil) {
-        self.setTextWithLineHeight(text: text, lineHeight: font.lineHeight())
+        self.setTextWithLineHeight(text: text, font: font)
         self.font = .jobisFont(font)
         guard let color else { return }
         self.textColor = color
@@ -10,15 +10,15 @@ public extension UILabel {
 }
 
 extension UILabel {
-     func setTextWithLineHeight(text: String?, lineHeight: CGFloat) {
+    func setTextWithLineHeight(text: String?, font: JobisFontStyle) {
         if let text = text {
             let style = NSMutableParagraphStyle()
-            style.maximumLineHeight = lineHeight
-            style.minimumLineHeight = lineHeight
+            style.maximumLineHeight = font.lineHeight()
+            style.minimumLineHeight = font.lineHeight()
 
             let attributes: [NSAttributedString.Key: Any] = [
                 .paragraphStyle: style,
-                .baselineOffset: (lineHeight - font.lineHeight) / 4
+                .baselineOffset: (font.lineHeight() - font.size()) / 4
             ]
 
             let attrString = NSAttributedString(string: text,
