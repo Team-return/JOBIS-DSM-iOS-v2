ab4dc4d2b2b2a83f2e1f78a342eaaf1cac4783cb
juyeong525 <juyeongno1@naver.com>
2023-11-10 09:49:35 +0900

    feat :: Signin 퍼블리싱

--- Projects/Presentation/Sources/Signin/SigninViewController.swift
+++ Projects/Presentation/Sources/Signin/SigninViewController.swift
@@ -7,15 +7,37 @@ import Core
 import DesignSystem
 
 public class SigninViewController: BaseViewController<SigninViewModel> {
-    private let label = UILabel().then {
-        $0.setJobisText("로그인", font: .body, color: .black)
+    private let titleLabel = UILabel().then {
+        $0.setJobisText("JOBIS에 로그인하기", font: .pageTitle, color: .GrayScale.gray90, range: "JOBIS")
+    }
+    private let titleLineView = UIView().then {
+        $0.backgroundColor = .GrayScale.gray90
+    }
+    private let titleImageView = UIImageView().then {
+        $0.image = .jobisIcon(.door)
     }
     public override func addView() {
-        self.view.addSubview(label)
+        [
+            titleLabel,
+            titleLineView,
+            titleImageView
+        ].forEach { self.view.addSubview($0) }
     }
     public override func layout() {
-        label.snp.makeConstraints {
-            $0.center.equalToSuperview()
+        titleLabel.snp.makeConstraints {
+            $0.topMargin.equalToSuperview().inset(20)
+            $0.leading.equalToSuperview().inset(24)
+        }
+        titleLineView.snp.makeConstraints {
+            $0.centerY.equalTo(titleLabel.snp.centerY)
+            $0.leading.equalTo(titleLabel.snp.trailing).offset(12)
+            $0.trailing.equalTo(titleImageView.snp.leading).offset(-12)
+            $0.height.equalTo(1)
+        }
+        titleImageView.snp.makeConstraints {
+            $0.centerY.equalTo(titleLabel.snp.centerY)
+            $0.width.height.equalTo(32)
+            $0.trailing.equalToSuperview().inset(24)
         }
     }
     public override func attribute() {
