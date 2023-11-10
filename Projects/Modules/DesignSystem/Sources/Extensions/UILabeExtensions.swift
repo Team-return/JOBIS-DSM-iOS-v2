e5ccb7ea9dda7dd79f6ddb7c7c5f59ed982bae9d
juyeong525 <juyeongno1@naver.com>
2023-11-10 09:49:35 +0900

    feat :: Signin 퍼블리싱

--- Projects/Modules/DesignSystem/Sources/Extensions/UILabeExtensions.swift
+++ Projects/Modules/DesignSystem/Sources/Extensions/UILabeExtensions.swift
@@ -1,16 +1,15 @@
 import UIKit
 
 public extension UILabel {
-    func setJobisText(_ text: String, font: JobisFontStyle, color: UIColor? = nil) {
-        self.setTextWithLineHeight(text: text, font: font)
+    func setJobisText(_ text: String, font: JobisFontStyle, color: UIColor? = nil, range: String? = nil) {
+        self.setTextWithLineHeight(text: text, font: font, color: color, range: range)
         self.font = .jobisFont(font)
         guard let color else { return }
-        self.textColor = color
     }
 }
 
 extension UILabel {
-    func setTextWithLineHeight(text: String?, font: JobisFontStyle) {
+    func setTextWithLineHeight(text: String?, font: JobisFontStyle, color: UIColor?, range: String?) {
         if let text = text {
             let style = NSMutableParagraphStyle()
             style.maximumLineHeight = font.lineHeight()
@@ -18,11 +17,16 @@ extension UILabel {
 
             let attributes: [NSAttributedString.Key: Any] = [
                 .paragraphStyle: style,
-                .baselineOffset: (font.lineHeight() - font.size()) / 4
+                .baselineOffset: (font.lineHeight() - font.size()) / 4,
+                .foregroundColor: color ?? .GrayScale.gray90
             ]
 
-            let attrString = NSAttributedString(string: text,
-                                                attributes: attributes)
+            let attrString = NSMutableAttributedString(string: text,
+                                                       attributes: attributes)
+            attrString.addAttribute(.foregroundColor,
+                                    value: UIColor.Main.blue1,
+                                    range: (text as NSString).range(of: range ?? "")
+            )
             self.attributedText = attrString
         }
     }
