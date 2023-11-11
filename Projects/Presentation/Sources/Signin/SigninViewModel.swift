2a6f1af0b7ae3cd3e66cdec37c324bfd7d1ccb0b
hsj._.06 <gtw030488@gmail.com>
2023-11-12 00:07:56 +0900

    feat :: signinViewModel 대충짜봄.. help..

--- Projects/Presentation/Sources/Signin/SigninViewModel.swift
+++ Projects/Presentation/Sources/Signin/SigninViewModel.swift
@@ -1,4 +1,5 @@
 import Core
+import Domain
 import RxFlow
 import RxSwift
 import RxCocoa
@@ -6,11 +7,49 @@ import RxCocoa
 public final class SigninViewModel: BaseViewModel, Stepper {
     public var steps = PublishRelay<Step>()
 
+    private let signinUseCase: SigninUseCase
+
+    init(signinUseCase: SigninUseCase) {
+        self.signinUseCase = signinUseCase
+    }
+
     private let disposeBag = DisposeBag()
-    public struct Input { }
-    public struct Output { }
+    public struct Input {
+        let email: Driver<String>
+        let password: Driver<String>
+        let signinButtonDidTap: Signal<Void>
+    }
+    public struct Output {
+        let emailErrorDescription: PublishRelay<String>
+        let passwordErrorDescription: PublishRelay<String>
+    }
 
     public func transform(_ input: Input) -> Output {
-        return Output()
+        let emailErrorDescription: PublishRelay<String>
+        let passwordErrorDescription: PublishRelay<String>
+
+        let info = Driver.combineLatest(input.email, input.password)
+        input.signinButtonDidTap.asObservable()
+            .withLatestFrom(info)
+            .flatMap { email, password in
+                self.signinUseCase.execute(req: .init(accountID: email, password: password))
+                    .asObservable()
+                    .filter { $0 == .developer || $0 == .student }
+                    .
+            }
+            .subscribe(onNext: { _ in
+                Single.just(SigninStep.mainIsRequired)
+            }, onError: { error in
+                guard let error = error as? UsersError else { return }
+                switch error {
+                case .notFoundPassword:
+                    passwordErrorDescription.accept(error.localizedDescription)
+                case .notFoundEmail, .internalServerError:
+                    emailErrorDescription.accept(error.localizedDescription)
+                }
+            })
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+        return Output(emailErrorDescription: emailErrorDescription, passwordErrorDescription: passwordErrorDescription)
     }
 }
