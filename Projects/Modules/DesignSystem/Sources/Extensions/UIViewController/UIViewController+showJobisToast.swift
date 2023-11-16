757dad71fdca31577cb4b6b05d105af7f9a0c071
HongSJae <gtw030488@gmail.com>
2023-11-17 08:38:42 +0900

    feat :: toast Animation 수정

--- Projects/Modules/DesignSystem/Sources/Extensions/UIViewController/UIViewController+showJobisToast.swift
+++ Projects/Modules/DesignSystem/Sources/Extensions/UIViewController/UIViewController+showJobisToast.swift
@@ -5,21 +5,29 @@ import SnapKit
 public extension UIViewController {
     func showJobisToast(text: String) {
         let jobisToast = JobisToast(text: text)
-        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut) { [self] in
-            setToastLayout(toast: jobisToast)
+        let animationOption = UIView.AnimationOptions.transitionCrossDissolve
+        self.view.addSubview(jobisToast)
+        jobisToast.snp.makeConstraints(hiddeToast)
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.3, delay: 0, options: animationOption) { [self] in
+            jobisToast.alpha = 1
+            jobisToast.snp.remakeConstraints {
+                $0.centerX.equalToSuperview()
+                $0.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(12).priority(1000)
+            }
+            self.view.layoutIfNeeded()
         }
-        UIView.animate(withDuration: 2, delay: 2.5, options: .curveEaseOut, animations: {
-            jobisToast.alpha = 0.0
+        UIView.animate(withDuration: 0.3, delay: 3, options: animationOption, animations: { [self] in
+            jobisToast.alpha = 0
+            jobisToast.snp.remakeConstraints(hiddeToast)
+            self.view.layoutIfNeeded()
         }, completion: { _ in
             jobisToast.removeFromSuperview()
         })
     }
 
-    private func setToastLayout(toast jobisToast: JobisToast) {
-        self.view.addSubview(jobisToast)
-        jobisToast.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(12).priority(1000)
-        }
+    private func hiddeToast(view: ConstraintMaker) {
+        view.centerX.equalToSuperview()
+        view.top.equalTo(self.view.snp.bottom)
     }
 }
