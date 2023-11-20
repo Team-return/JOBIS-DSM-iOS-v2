05f837bab41407f83ead262dca62d79175243200
HongSJae <gtw030488@gmail.com>
2023-11-20 09:11:33 +0900

    refactoring :: ifNeeded 위치 수정

--- Projects/Modules/DesignSystem/Sources/Extensions/UIViewController/UIViewController+showJobisToast.swift
+++ Projects/Modules/DesignSystem/Sources/Extensions/UIViewController/UIViewController+showJobisToast.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Then
 import SnapKit
 
 public extension UIViewController {
@@ -7,8 +6,8 @@ public extension UIViewController {
         let jobisToast = JobisToast(text: text)
         let animationOption = UIView.AnimationOptions.transitionCrossDissolve
         self.view.addSubview(jobisToast)
-        jobisToast.snp.makeConstraints(hiddeToast)
-        self.view.layoutIfNeeded()
+        resetToast(toast: jobisToast)
+
         UIView.animate(withDuration: 0.3, delay: 0, options: animationOption) { [self] in
             jobisToast.alpha = 1
             jobisToast.snp.remakeConstraints {
@@ -17,17 +16,20 @@ public extension UIViewController {
             }
             self.view.layoutIfNeeded()
         }
+
         UIView.animate(withDuration: 0.3, delay: 3, options: animationOption, animations: { [self] in
             jobisToast.alpha = 0
-            jobisToast.snp.remakeConstraints(hiddeToast)
-            self.view.layoutIfNeeded()
+            resetToast(toast: jobisToast)
         }, completion: { _ in
             jobisToast.removeFromSuperview()
         })
     }
 
-    private func hiddeToast(view: ConstraintMaker) {
-        view.centerX.equalToSuperview()
-        view.top.equalTo(self.view.snp.bottom)
+    private func resetToast(toast: JobisToast) {
+        self.view.layoutIfNeeded()
+        toast.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(self.view.snp.bottom)
+        }
     }
 }
