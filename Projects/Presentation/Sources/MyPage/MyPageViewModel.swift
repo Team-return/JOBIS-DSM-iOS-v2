b4823f111b84b3aa7b0fec2e1ffa62c8c1ba8aaf
juyeong525 <juyeongno1@naver.com>
2023-11-21 23:37:34 +0900

    feat :: MyPageViewModel

--- Projects/Presentation/Sources/MyPage/MyPageViewModel.swift
+++ Projects/Presentation/Sources/MyPage/MyPageViewModel.swift
@@ -8,15 +8,28 @@ import Domain
 public class MyPageViewModel: BaseViewModel, Stepper {
     public var steps = PublishRelay<Step>()
 
+    private let fetchStudentInfoUseCase: FetchStudentInfoUseCase
+
+    init(fetchStudentInfoUseCase: FetchStudentInfoUseCase) {
+        self.fetchStudentInfoUseCase = fetchStudentInfoUseCase
+    }
+
     private let disposeBag = DisposeBag()
 
     public struct Input {
+        let viewAppear: PublishRelay<Void>
     }
 
     public struct Output {
+        let studentInfo: PublishRelay<StudentInfoEntity>
     }
 
     public func transform(_ input: Input) -> Output {
-        return Output()
+        let studentInfo = PublishRelay<StudentInfoEntity>()
+        input.viewAppear.asObservable()
+            .flatMap { self.fetchStudentInfoUseCase.execute() }
+            .bind(to: studentInfo)
+            .disposed(by: disposeBag)
+        return Output(studentInfo: studentInfo)
     }
 }
