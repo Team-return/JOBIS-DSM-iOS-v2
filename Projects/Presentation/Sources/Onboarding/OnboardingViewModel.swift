3e6ec3c314e457a10da0f24c3a3482275c247a1f
juyeong525 <juyeongno1@naver.com>
2023-11-22 01:06:38 +0900

    refactoring :: 자동로그인 & 스플래시 구현

--- Projects/Presentation/Sources/Onboarding/OnboardingViewModel.swift
+++ Projects/Presentation/Sources/Onboarding/OnboardingViewModel.swift
@@ -1,4 +1,5 @@
 import Core
+import Domain
 import RxFlow
 import RxSwift
 import RxCocoa
@@ -6,16 +7,25 @@ import RxCocoa
 public final class OnboardingViewModel: BaseViewModel, Stepper {
     public var steps = PublishRelay<Step>()
 
+    private let reissueTokenUaseCase: ReissueTokenUaseCase
+
     private let disposeBag = DisposeBag()
+    init(reissueTokenUaseCase: ReissueTokenUaseCase) {
+        self.reissueTokenUaseCase = reissueTokenUaseCase
+    }
 
     public struct Input {
         let navigateToSigninDidTap: Signal<Void>
         let navigateToSignupDidTap: Signal<Void>
+        let viewAppear: PublishRelay<Void>
     }
 
-    public struct Output { }
+    public struct Output {
+        let animation: PublishRelay<Void>
+    }
 
     public func transform(_ input: Input) -> Output {
+        let animation = PublishRelay<Void>()
         input.navigateToSigninDidTap.asObservable()
             .map { _ in OnboardingStep.signinIsRequired }
             .bind(to: steps)
@@ -25,6 +35,20 @@ public final class OnboardingViewModel: BaseViewModel, Stepper {
             .map { _ in OnboardingStep.signupIsRequired }
             .bind(to: steps)
             .disposed(by: disposeBag)
-        return Output()
+        input.viewAppear.asObservable()
+            .flatMap { [self] in
+                reissueTokenUaseCase.execute()
+                    .asCompletable()
+                    .catch { _ in
+                        print("Sdf")
+                        animation.accept(())
+                        return .never()
+                    }
+                    .andThen(Single.just(OnboardingStep.tabsIsRequired))
+            }
+            .delay(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+        return Output(animation: animation)
     }
 }
