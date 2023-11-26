5f3ca607ba6fb5e19283d1e264ffa3c148f86a4e
HongSJae <gtw030488@gmail.com>
2023-11-26 23:17:30 +0900

    feat :: InfoSettingFlow

---
+++ Projects/Flow/Sources/Signup/InfoSettingFlow.swift
@@ -0,0 +1,53 @@
+import UIKit
+import Presentation
+import Swinject
+import RxFlow
+import Core
+
+public class InfoSettingFlow: Flow {
+    public var container: Container
+
+    public var root: Presentable {
+        return rootViewController
+    }
+    public init(container: Container) {
+        self.container = container
+        self.rootViewController = container.resolve(InfoSettingViewController.self)!
+    }
+
+    private let rootViewController: InfoSettingViewController
+
+    public func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? InfoSettingStep else { return .none }
+
+        switch step {
+        case .infoSettingIsRequired:
+            return navigateToInfoSetting()
+        case .verifyEmailIsRequired:
+            return navigateToVerifyEmail()
+        case .tabsIsRequired:
+            return .end(forwardToParentFlowWithStep: OnboardingStep.tabsIsRequired)
+        }
+    }
+}
+
+private extension InfoSettingFlow {
+    func navigateToInfoSetting() -> FlowContributors {
+        print("here is info setting")
+        return .one(flowContributor: .contribute(
+            withNextPresentable: rootViewController,
+            withNextStepper: rootViewController.viewModel
+        ))
+    }
+
+    func navigateToVerifyEmail() -> FlowContributors {
+        let verifyEmailFlow = VerifyEmailFlow(container: container)
+        Flows.use(verifyEmailFlow, when: .created) { root in
+            self.rootViewController.navigationController?.pushViewController(root, animated: true)
+        }
+        return .one(flowContributor: .contribute(
+            withNextPresentable: verifyEmailFlow,
+            withNextStepper: OneStepper(withSingleStep: VerifyEmailStep.verifyEmailIsRequired)
+        ))
+    }
+}
