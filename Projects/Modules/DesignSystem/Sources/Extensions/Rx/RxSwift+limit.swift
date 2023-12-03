700b4340eab84b156ef65a51164a3d6b84d92075
HongSJae <gtw030488@gmail.com>
2023-12-03 17:25:32 +0900

    feat :: 글자수 제한 Rx Extension 추가

---
+++ Projects/Modules/DesignSystem/Sources/Extensions/Rx/RxSwift+limit.swift
@@ -0,0 +1,12 @@
+import RxSwift
+import UIKit
+import RxCocoa
+
+public extension Observable<String> {
+    func limit(_ max: Int, onComplete: @escaping () -> Void) -> Observable<String> {
+        scan("") { old, new in
+            if old.count == max - 1 && new.count == max { onComplete() }
+            return new.count <= max || new.isEmpty ? new: old
+        }
+    }
+}
