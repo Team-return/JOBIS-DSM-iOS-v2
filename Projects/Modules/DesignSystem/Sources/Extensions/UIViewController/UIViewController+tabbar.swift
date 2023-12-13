758111298de2a690bab5b7086bcd40d775e0df4f
hsj._.06 <gtw030488@gmail.com>
2023-12-13 16:01:26 +0900

    🧩 :: Show/hide Tabbar animation 수정

--- Projects/Modules/DesignSystem/Sources/Extensions/UIViewController/UIViewController+tabbar.swift
+++ Projects/Modules/DesignSystem/Sources/Extensions/UIViewController/UIViewController+tabbar.swift
@@ -2,11 +2,8 @@ import UIKit
 
 public extension UIViewController {
     func showTabbar() {
-        UIView.animate(
-            withDuration: 0.3,
-            delay: 0,
-            options: .transitionCrossDissolve
-        ) { [weak self] in
+        UIView.tabbarAnimate { [weak self] in
+            self?.tabBarController?.tabBar.alpha = 1
             guard let tabBarFrame = self?.tabBarController?.tabBar.frame else { return }
             self?.tabBarController?.tabBar.frame.origin.y -= (tabBarFrame.height * 2)
             self?.navigationController?.view.layoutIfNeeded()
@@ -16,14 +13,26 @@ public extension UIViewController {
     }
 
     func hideTabbar() {
-        UIView.animate(
-            withDuration: 0.3,
-            delay: 0,
-            options: .transitionCrossDissolve
-        ) { [weak self] in
+        UIView.tabbarAnimate { [weak self] in
+            self?.tabBarController?.tabBar.alpha = 0
             guard let tabBarFrame = self?.tabBarController?.tabBar.frame else { return }
             self?.tabBarController?.tabBar.frame.origin.y = tabBarFrame.maxY + tabBarFrame.height
             self?.navigationController?.view.layoutIfNeeded()
         }
+    }
+}
+
+private extension UIView {
+    static func tabbarAnimate(
+        animations: @escaping () -> Void,
+        completion: ((Bool) -> Void)? = nil
+    ) {
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0,
+            options: .transitionCrossDissolve,
+            animations: animations,
+            completion: completion
+        )
     }
 }
