00e96fae715448f3bf2ab6ff44d42bf65b1025c6
hsj._.06 <gtw030488@gmail.com>
2023-12-13 19:47:35 +0900

    🛠️ :: 로직 이상한 거 수정

--- Projects/Presentation/Sources/Home/Components/CareerNavigationCard.swift
+++ Projects/Presentation/Sources/Home/Components/CareerNavigationCard.swift
@@ -14,12 +14,16 @@ enum CardSize {
 }
 
 final class CareerNavigationCard: UIButton {
-    private var headerLabelTexts = [String]()
-    private var iconView = UIView().then {
-        $0.backgroundColor = .GrayScale.gray10
-        $0.asCircle()
+    private let headerLabel = UILabel().then {
+        $0.numberOfLines = 0
     }
-    private var icon = UIImageView()
+    private let iconView = UIView().then {
+        $0.backgroundColor = .GrayScale.gray10
+        $0.layer.cornerRadius = 32
+        $0.isUserInteractionEnabled = false
+        $0.clipsToBounds = true
+    }
+    private let icon = UIImageView()
 
     public init(
         style: CardSize
@@ -28,55 +32,46 @@ final class CareerNavigationCard: UIButton {
         self.backgroundColor = .GrayScale.gray30
         self.layer.cornerRadius = 12
 
-        var info: (textArr: [String], icon: JobisIcon) {
-            switch style {
-            case let .small(type):
-                switch type {
-                case .findCompanys:
-                    return (["다른 기업들", "탐색 하기 →"], .officeBuilding)
-                case .findWinterRecruitments:
-                    return (["체험형", "현장실습 →"], .snowman)
-                }
-            case .large:
-                return (["다른 기업들은 어떨까?", "다른 기업들 둘러 보러 가기 →"], .officeBuilding)
-            }
-        }
-
-        headerLabelTexts = info.textArr
-        icon.image = .jobisIcon(info.icon).resize(.init(width: 40, height: 40))
+        setCard(style: style)
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func layoutSubviews() {
-        let headerLabels: [UILabel] = headerLabelTexts.map { text in
-            let label = UILabel()
-            label.setJobisText(text, font: .headLine, color: .GrayScale.gray90)
-            return label
-        }
-        (headerLabels + [iconView]).forEach(addSubview(_:))
-        iconView.addSubview(icon)
-
-        headerLabels.indices.forEach { index in
-            if headerLabels[index] == headerLabels.first {
-                headerLabels[index].snp.makeConstraints {
-                    $0.top.leading.equalToSuperview().offset(20)
+    private func setCard(style: CardSize) {
+        var info: (text: String, icon: JobisIcon) {
+            switch style {
+            case let .small(type):
+                switch type {
+                case .findCompanys:
+                    return ("다른 기업들\n탐색 하기 →", .officeBuilding)
+                case .findWinterRecruitments:
+                    return ("체험형\n현장실습 →", .snowman)
                 }
-            } else {
-                headerLabels[index].snp.makeConstraints {
-                    $0.top.equalTo(headerLabels[index - 1].snp.bottom)
-                    $0.leading.equalTo(headerLabels[index - 1])
-                }
+            case .large:
+                return ("다른 기업들은 어떨까?\n다른 기업들 둘러 보러 가기 →", .officeBuilding)
             }
         }
-        iconView.snp.makeConstraints {
-            $0.top.equalTo(headerLabels.last!.snp.bottom).offset(16)
-            $0.bottom.trailing.equalToSuperview().inset(20)
+
+        headerLabel.setJobisText(info.text, font: .headLine, color: .GrayScale.gray90)
+        icon.image = .jobisIcon(info.icon)
+            .resize(.init(width: 40, height: 40))
+    }
+
+    override func layoutSubviews() {
+        [iconView, headerLabel].forEach(addSubview(_:))
+        iconView.addSubview(icon)
+
+        headerLabel.snp.makeConstraints {
+            $0.leading.top.equalToSuperview().inset(20)
         }
-        iconView.asCircle()
         icon.snp.makeConstraints {
-            $0.edges.equalToSuperview().inset(12)
+            $0.center.equalToSuperview()
         }
+        iconView.snp.makeConstraints {
+            $0.bottom.trailing.equalToSuperview().inset(20)
+            $0.width.height.trailing.equalTo(64)
+        }
+        iconView.layoutIfNeeded()
     }
 }
