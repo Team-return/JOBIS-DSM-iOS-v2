becd0f654c7cd727d0552d5a35c4161674a1392b
ray3238 <jjmh209@gmail.com>
2023-12-14 17:43:08 +0900

    🛠 :: tableViewCell BackgroundColor 수정

--- Projects/Presentation/Sources/Recruitment/RecruitmentTableViewCell.swift
+++ Projects/Presentation/Sources/Recruitment/RecruitmentTableViewCell.swift
@@ -45,7 +45,7 @@ final class RecruitmentTableViewCell: UITableViewCell {
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .white
+        backgroundColor = UIColor.GrayScale.gray10
         addView()
         layout()
         bookmarkStatus()
