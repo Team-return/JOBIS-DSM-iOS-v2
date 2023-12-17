2ea10b78a67a1c66a00aa50110959931aea32f39
HongSJae <gtw030488@gmail.com>
2023-12-17 14:55:55 +0900

    🛠️ :: Lib Dependency baseSetting

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -2,43 +2,52 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let dependencies = Dependencies(
-    swiftPackageManager: .init([
-        .remote(
-            url: "https://github.com/SnapKit/SnapKit.git",
-            requirement: .upToNextMinor(from: "5.0.0")
-        ),
-        .remote(
-            url: "https://github.com/devxoul/Then.git",
-            requirement: .upToNextMajor(from: "3.0.0")
-        ),
-        .remote(
-            url: "https://github.com/ReactiveX/RxSwift.git",
-            requirement: .upToNextMinor(from: "6.5.0")
-        ),
-        .remote(
-            url: "https://github.com/RxSwiftCommunity/RxFlow.git",
-            requirement: .upToNextMajor(from: "2.13.0")
-        ),
-        .remote(
-            url: "https://github.com/Swinject/Swinject.git",
-            requirement: .upToNextMajor(from: "2.8.3")
-        ),
-        .remote(
-            url: "https://github.com/Moya/Moya.git",
-            requirement: .upToNextMajor(from: "15.0.0")
-        ),
-        .remote(
-            url: "https://github.com/airbnb/lottie-ios",
-            requirement: .upToNextMajor(from: "4.3.3")
-        ),
-        .remote(
-            url: "https://github.com/onevcat/Kingfisher",
-            requirement: .upToNextMajor(from: "7.0.0")
-        ),
-        .remote(
-            url: "https://github.com/evgenyneu/keychain-swift",
-            requirement: .upToNextMajor(from: "20.0.0")
+    swiftPackageManager: .init(
+        [
+            .remote(
+                url: "https://github.com/SnapKit/SnapKit.git",
+                requirement: .upToNextMinor(from: "5.0.0")
+            ),
+            .remote(
+                url: "https://github.com/devxoul/Then.git",
+                requirement: .upToNextMajor(from: "3.0.0")
+            ),
+            .remote(
+                url: "https://github.com/ReactiveX/RxSwift.git",
+                requirement: .upToNextMinor(from: "6.5.0")
+            ),
+            .remote(
+                url: "https://github.com/RxSwiftCommunity/RxFlow.git",
+                requirement: .upToNextMajor(from: "2.13.0")
+            ),
+            .remote(
+                url: "https://github.com/Swinject/Swinject.git",
+                requirement: .upToNextMajor(from: "2.8.3")
+            ),
+            .remote(
+                url: "https://github.com/Moya/Moya.git",
+                requirement: .upToNextMajor(from: "15.0.0")
+            ),
+            .remote(
+                url: "https://github.com/airbnb/lottie-ios",
+                requirement: .upToNextMajor(from: "4.3.3")
+            ),
+            .remote(
+                url: "https://github.com/onevcat/Kingfisher",
+                requirement: .upToNextMajor(from: "7.0.0")
+            ),
+            .remote(
+                url: "https://github.com/evgenyneu/keychain-swift",
+                requirement: .upToNextMajor(from: "20.0.0")
+            )
+        ],
+        baseSettings: .settings(
+            configurations: [
+                .debug(name: .dev),
+                .debug(name: .stage),
+                .release(name: .prod)
+            ]
         )
-    ]),
+    ),
     platforms: [.iOS]
 )
