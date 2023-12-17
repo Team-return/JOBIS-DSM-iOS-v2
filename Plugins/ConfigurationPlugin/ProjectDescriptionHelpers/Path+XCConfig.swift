d90c5aa4a3b388d233c612aaea9ef8c1a1ec8a3f
HongSJae <gtw030488@gmail.com>
2023-12-17 15:24:59 +0900

    🛠️ :: Path XCConfig 파일 return 지움

--- Plugins/ConfigurationPlugin/ProjectDescriptionHelpers/Path+XCConfig.swift
+++ Plugins/ConfigurationPlugin/ProjectDescriptionHelpers/Path+XCConfig.swift
@@ -2,6 +2,6 @@ import ProjectDescription
 
 public extension ProjectDescription.Path {
     static func relativeToXCConfig(type: ProjectDeployTarget, name: String) -> Self {
-        return .relativeToRoot("XCConfig/\(name)/\(type.rawValue).xcconfig")
+         .relativeToRoot("XCConfig/\(name)/\(type.rawValue).xcconfig")
     }
 }
