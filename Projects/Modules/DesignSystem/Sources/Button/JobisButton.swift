7237d895c7c04e8814d33684684578af6cd4e977
hsj._.06 <gtw030488@gmail.com>
2023-12-24 12:30:45 +0900

    🧩 :: Jobis Button 에 색 적용

--- Projects/Modules/DesignSystem/Sources/Button/JobisButton.swift
+++ Projects/Modules/DesignSystem/Sources/Button/JobisButton.swift
@@ -4,46 +4,40 @@ import RxCocoa
 import Then
 import SnapKit
 
+// TODO: configureUI 라는 함수가 너무 커서 isEnabled와 isHighlighted 등 didset에 들어가있으면 값변화가 너무 클 듯 함수를 좀 더 세분화 하는 방향으로..
 public final class JobisButton: UIButton {
     private var buttonStyle: JobisButtonStyle = .main
     private var labelText: String = ""
 
     public override var isHighlighted: Bool {
-        didSet {
-            super.isHighlighted = isHighlighted
-
-            self.configureUI()
-        }
+        didSet { self.configureUI() }
     }
 
     public override var isEnabled: Bool {
-        didSet {
-            super.isEnabled = isEnabled
-
-            self.configureUI()
-        }
+        didSet { self.configureUI() }
     }
 
-    private func backgroundColor() -> UIColor {
+    private var fgColor: UIColor {
+        guard traitCollection.userInterfaceStyle == .light else { return .black }
+        return (buttonStyle == .main || !isEnabled) ? .white: .black
+    }
+
+    private var buttonImage: JobisIcon {
+        guard traitCollection.userInterfaceStyle == .light else { return .arrowRight(.black) }
+        return (buttonStyle == .main || !isEnabled) ? .arrowRight(.white): .arrowRight(.black)
+    }
+
+    private var bgColor: UIColor {
+        let isDark = traitCollection.userInterfaceStyle == .dark
         if isEnabled {
             switch buttonStyle {
             case .main:
-                return isHighlighted ? .Main.blue3: .Main.blue1
+                return isDark ? .Primary.blue20: isHighlighted ? .Primary.blue40: .Primary.blue20
             case .sub:
                 return isHighlighted ? .GrayScale.gray40: .GrayScale.gray30
             }
         } else {
             return .GrayScale.gray50
-        }
-    }
-
-    private func foregroundColor() -> UIColor {
-        let isDarkMode = traitCollection.userInterfaceStyle == .dark
-
-        if !isEnabled || buttonStyle == .main {
-            return isDarkMode ? .GrayScale.gray90: .GrayScale.gray10
-        } else {
-            return .GrayScale.gray90
         }
     }
 
@@ -66,9 +60,7 @@ public final class JobisButton: UIButton {
     }
 
     private func configureUI() {
-        let fgColor = foregroundColor()
-        let image: UIImage? = .jobisIcon(.arrowRight)
-            .withTintColor(fgColor, renderingMode: .alwaysOriginal)
+        let image: UIImage? = buttonImage.uiImage()
             .resize(.init(width: 24, height: 24))
 
         var config = UIButton.Configuration.plain()
@@ -82,6 +74,19 @@ public final class JobisButton: UIButton {
 
         self.configuration = config
         self.layer.cornerRadius = 12
-        self.backgroundColor = backgroundColor()
+        self.backgroundColor = bgColor
+    }
+
+    private func resetUI() {
+        self.imageView?.image = buttonImage.uiImage()
+            .resize(.init(width: 24, height: 24))
+        self.titleLabel?.textColor = fgColor
+        self.backgroundColor = bgColor
+    }
+}
+
+extension JobisButton {
+    public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        resetUI()
     }
 }
