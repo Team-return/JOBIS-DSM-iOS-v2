c4c2739273015b12d79fe9ba0420c76ea9bca216
hsj._.06 <gtw030488@gmail.com>
2023-12-27 08:46:32 +0900

    🛠️ :: 변수명 리팩토링

--- Projects/Presentation/Sources/MyPage/Components/ReviewNavigateStackView.swift
+++ Projects/Presentation/Sources/MyPage/Components/ReviewNavigateStackView.swift
@@ -7,14 +7,16 @@ import RxCocoa
 final class ReviewNavigateStackView: UIStackView {
     let reviewNavigateButtonDidTap = PublishRelay<Int>()
     private let disposeBag = DisposeBag()
+
     init() {
         super.init(frame: .zero)
         self.spacing = 12
         self.isLayoutMarginsRelativeArrangement = true
         self.axis = .vertical
     }
-    func setList(list: [WritableReviewCompanyEntity]) {
-        if list.isEmpty {
+
+    func setList(writableReviewCompanylist: [WritableReviewCompanyEntity]) {
+        if writableReviewCompanylist.isEmpty {
             self.layoutMargins = .init(top: 0, left: 0, bottom: 0, right: 0)
         } else {
             self.layoutMargins = .init(top: 12, left: 0, bottom: 12, right: 0)
@@ -24,10 +26,10 @@ final class ReviewNavigateStackView: UIStackView {
             NSLayoutConstraint.deactivate($0.constraints)
             $0.removeFromSuperview()
         }
-        list.forEach { item in
+        writableReviewCompanylist.forEach { writableReviewCompany in
             let reviewNavigateTableViewCell = ReviewNavigateView().then {
-                $0.titleLabel.text = "\(item.name) 면접 후기를 적어주세요!"
-                $0.reviewID = item.reviewID
+                $0.titleLabel.text = "\(writableReviewCompany.name) 면접 후기를 적어주세요!"
+                $0.reviewID = writableReviewCompany.reviewID
             }
             reviewNavigateTableViewCell.reviewNavigateButton.rx.tap
                 .bind(onNext: { [weak self] in
