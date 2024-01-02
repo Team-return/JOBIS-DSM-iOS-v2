34538cd7b94eb7b6566f39fd4d8861dc4c59294a
hsj._.06 <gtw030488@gmail.com>
2024-01-02 14:48:08 +0900

    🧩 :: Signin에 platformType추가

--- Projects/Domain/Sources/Parameters/Users/SigninRequestQuery.swift
+++ Projects/Domain/Sources/Parameters/Users/SigninRequestQuery.swift
@@ -3,14 +3,17 @@ import Foundation
 public struct SigninRequestQuery: Encodable {
     public let accountID: String
     public let password: String
+    public let platformType: String
 
     public init(accountID: String, password: String) {
         self.accountID = accountID
         self.password = password
+        self.platformType = "iOS"
     }
 
     enum CodingKeys: String, CodingKey {
         case accountID = "account_id"
         case password
+        case platformType = "platform_type"
     }
 }
