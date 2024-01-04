75dad7e713f14d6c2b04e72f2dddaf2d0c9872d2
hsj._.06 <gtw030488@gmail.com>
2024-01-04 14:22:13 +0900

    🧩 :: BugSectionView

---
+++ Projects/Presentation/Sources/MyPage/Components/BugSectionView.swift
@@ -0,0 +1,31 @@
+import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+import DesignSystem
+
+final class BugSectionView: UIView {
+    enum BugSectionType: Int {
+        case reportBug
+        case bugList
+    }
+    private let bugSectionView = SectionView(menuText: "버그제보").then {
+        $0.setSection(items: [
+            ("버그 제보하기", .jobisIcon(.bugReport)),
+            ("버그 제보함", .jobisIcon(.bugBox))
+        ])
+    }
+
+    override func layoutSubviews() {
+        self.addSubview(bugSectionView)
+
+        bugSectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    func getSelectedItem(type: BugSectionType) -> Observable<IndexPath> {
+        self.bugSectionView.getSelectedItem(index: type.rawValue)
+    }
+}
