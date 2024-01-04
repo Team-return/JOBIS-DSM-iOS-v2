368b3937280986c0ba185cd630b089bf8be2bfc8
juyeongno1 <juyeongno1@naver.com>
2024-01-04 12:10:28 +0900

    🧩 :: 공백 추가

--- Projects/Presentation/Sources/MyPage/MyPageViewModel.swift
+++ Projects/Presentation/Sources/MyPage/MyPageViewModel.swift
@@ -39,19 +39,23 @@ public final class MyPageViewModel: BaseViewModel, Stepper {
     public func transform(_ input: Input) -> Output {
         let studentInfo = PublishRelay<StudentInfoEntity>()
         let writableReviewList = BehaviorRelay<[WritableReviewCompanyEntity]>(value: [])
+
         input.viewAppear.asObservable()
             .flatMap { self.fetchStudentInfoUseCase.execute() }
             .bind(to: studentInfo)
             .disposed(by: disposeBag)
+
         input.viewAppear.asObservable()
             .flatMap { self.fetchWritableReviewListUseCase.execute() }
             .bind(to: writableReviewList)
             .disposed(by: disposeBag)
+
         input.reviewNavigate.asObservable()
             .subscribe(onNext: {
                 // TODO: 리뷰 리스트로 네비게이션 이동 해주는 코드 았어야함
                 print($0)
             }).disposed(by: disposeBag)
+
         input.logoutSectionDidTap
             .do(onNext: { _ in
                 self.logoutUseCase.execute()
@@ -59,13 +63,15 @@ public final class MyPageViewModel: BaseViewModel, Stepper {
             .map { _ in MyPageStep.tabsIsRequired }
             .bind(to: steps)
             .disposed(by: disposeBag)
+
         input.withdrawalSectionDidTap
-            .do(onNext: { _ in 
+            .do(onNext: { _ in
                 self.logoutUseCase.execute()
             })
             .map { _ in MyPageStep.tabsIsRequired }
             .bind(to: steps)
             .disposed(by: disposeBag)
+
         return Output(studentInfo: studentInfo,
                       writableReviewList: writableReviewList)
     }
