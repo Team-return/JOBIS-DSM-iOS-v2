5e900b0511d9e93a23714b12c6f13787eb67f0a5
hsj._.06 <gtw030488@gmail.com>
2024-01-07 18:55:04 +0900

    🧩 :: Lottie 수정

--- Projects/Modules/DesignSystem/Sources/Lottie/JobisLottieView.swift
+++ Projects/Modules/DesignSystem/Sources/Lottie/JobisLottieView.swift
@@ -12,8 +12,7 @@ public class JobisLottieView: UIView {
 
     public init(_ type: JobisLottie) {
         super.init(frame: .zero)
-        self.lottieAnimationView = .init(name: "OnboardingLottie", bundle: .module)
-        configureView()
+        self.lottieAnimationView = .init(name: type.rawValue, bundle: .module)
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
