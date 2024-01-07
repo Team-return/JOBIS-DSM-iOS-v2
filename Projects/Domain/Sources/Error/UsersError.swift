08cfd64f993aecea37c223eb1f6f32d1f29113ac
juyeongno1 <juyeongno1@naver.com>
2024-01-07 23:50:11 +0900

    🧩 :: UsersError 추가

--- Projects/Domain/Sources/Error/UsersError.swift
+++ Projects/Domain/Sources/Error/UsersError.swift
@@ -4,6 +4,7 @@ public enum UsersError: Error {
     case notFoundPassword
     case notFoundEmail
     case internalServerError
+    case badRequest
 }
 
 extension UsersError: LocalizedError {
@@ -11,13 +12,16 @@ extension UsersError: LocalizedError {
         switch self {
 
         case .notFoundPassword:
-            return "비밀번호를 확인해주세요."
+            return "비밀번호가 옳지 않아요"
 
         case .notFoundEmail:
-            return "계정을 찾을 수 없습니다. 다시 한 번 확인해주세요."
+            return "아이디를 찾지 못했어요"
 
         case .internalServerError:
             return "인터넷 환경을 확인해주세요."
+
+        case .badRequest:
+            return "비밀번호 형식에 맞지 않아요."
         }
     }
 }
