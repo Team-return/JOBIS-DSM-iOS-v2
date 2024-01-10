d2111908f99bfe6b7674bf9c5326ff64cf8d99c1
juyeongno1 <juyeongno1@naver.com>
2024-01-10 10:46:32 +0900

    🧩 :: 함수 호출 위치 변경

--- Projects/Presentation/Sources/Base/BaseView.swift
+++ Projects/Presentation/Sources/Base/BaseView.swift
@@ -6,13 +6,17 @@ public class BaseView: UIView,
                      ViewConfigurable {
     public init() {
         super.init(frame: .zero)
-        addView()
-        setLayout()
         configureView()
     }
 
     public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        addView()
+        setLayout()
     }
 
     public func addView() {}
