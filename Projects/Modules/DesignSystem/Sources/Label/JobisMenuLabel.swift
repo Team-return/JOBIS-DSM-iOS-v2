18d47cb99350a1d895dbb0babfa52afa4b71e551
juyeongno1 <juyeongno1@naver.com>
2024-01-22 01:17:32 +0900

    🧩 :: SubTitle 추가

--- Projects/Modules/DesignSystem/Sources/Label/JobisMenuLabel.swift
+++ Projects/Modules/DesignSystem/Sources/Label/JobisMenuLabel.swift
@@ -1,13 +1,29 @@
 import UIKit
 import SnapKit
+import Then
 
 public final class JobisMenuLabel: UIView {
     private let title = UILabel()
+    private let line = UIView().then {
+        $0.isHidden = true
+        $0.backgroundColor = .GrayScale.gray60
+    }
+    private let subTitle = UILabel().then {
+        $0.isHidden = true
+    }
 
-    public init(text: String) {
+    public init(text: String, subText: String? = nil) {
         super.init(frame: .zero)
 
-        title.setJobisText(text, font: .description, color: .GrayScale.gray60)
+        self.title.setJobisText(text, font: .description, color: .GrayScale.gray60)
+        guard let subText else { return }
+        self.subTitle.setJobisText(subText, font: .description, color: .GrayScale.gray60)
+        self.line.isHidden = false
+        self.subTitle.isHidden = false
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
         configureView()
     }
 
@@ -16,11 +32,27 @@ public final class JobisMenuLabel: UIView {
     }
 
     private func configureView() {
-        self.addSubview(title)
+        [
+            title,
+            line,
+            subTitle
+        ].forEach(self.addSubview(_:))
 
         title.snp.makeConstraints {
             $0.top.bottom.equalToSuperview().inset(8)
             $0.leading.equalToSuperview().inset(24)
+        }
+
+        line.snp.makeConstraints {
+            $0.leading.equalTo(title.snp.trailing).offset(8)
+            $0.trailing.equalTo(subTitle.snp.leading).offset(-8)
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+
+        subTitle.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(8)
+            $0.trailing.equalToSuperview().inset(24)
         }
     }
 }
