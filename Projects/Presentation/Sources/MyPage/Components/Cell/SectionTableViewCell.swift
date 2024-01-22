b2e34a2f92771aeea9649819500e5649f8da0813
juyeongno1 <juyeongno1@naver.com>
2024-01-23 04:22:33 +0900

    🧩 :: BaseTableViewCell로 변경

--- Projects/Presentation/Sources/MyPage/Components/Cell/SectionTableViewCell.swift
+++ Projects/Presentation/Sources/MyPage/Components/Cell/SectionTableViewCell.swift
@@ -1,22 +1,24 @@
 import UIKit
 import DesignSystem
 
-final class SectionTableViewCell: UITableViewCell {
+final class SectionTableViewCell: BaseTableViewCell<(String, UIImage)> {
     static let identifier = "SectionTableViewCell"
     private let sectionImageView = UIImageView()
     private let titleLabel = UILabel()
 
     override func setHighlighted(_ highlighted: Bool, animated: Bool) {
         super.setHighlighted(highlighted, animated: animated)
-        self.titleLabel.textColor = self.isHighlighted ? .GrayScale.gray90.withAlphaComponent(0.1) : .GrayScale.gray90
+        self.titleLabel.textColor = self.isHighlighted ? .GrayScale.gray50 : .GrayScale.gray90
     }
 
-    override func layoutSubviews() {
+    override func addView() {
         [
             sectionImageView,
             titleLabel
         ].forEach { self.addSubview($0) }
+    }
 
+    override func setLayout() {
         sectionImageView.snp.makeConstraints {
             $0.top.bottom.equalToSuperview().inset(12)
             $0.width.equalTo(28)
@@ -28,9 +30,9 @@ final class SectionTableViewCell: UITableViewCell {
         }
     }
 
-    func setCell(image: UIImage, title: String) {
-        self.sectionImageView.image = image
-        self.titleLabel.setJobisText(title, font: .body, color: .GrayScale.gray90)
+    override func adapt(model: (String, UIImage)) {
+        self.sectionImageView.image = model.1
+        self.titleLabel.setJobisText(model.0, font: .body, color: .GrayScale.gray90)
         self.selectionStyle = .none
     }
 }
