793ffce1ad8bc966275164c102283af6a4aa3e4e
hsj._.06 <gtw030488@gmail.com>
2024-03-07 13:46:18 +0900

    ♻️ :: CI 갈아엎음

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-sh ci_install_tuist.sh
-cd ../
+cd ..
+
 git clone https://github.com/Team-return/JOBIS-v2-XCConfig.git
 mv JOBIS-v2-XCConfig/XCConfig/ .
 
-ci_scripts/tuist/tuist fetch
-TUIST_CI=1 ci_scripts/tuist/tuist generate
+brew install make
+curl -Ls https://install.tuist.io | bash
+
+TUIST_CI=1
+make ci_generate
