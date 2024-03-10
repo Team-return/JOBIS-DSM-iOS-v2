b6fc57651597cd976c7f2899fef14155e81eb8e5
hsj._.06 <gtw030488@gmail.com>
2024-03-11 08:43:36 +0900

    🧩 :: Profile Setting Flow

---
+++ Projects/Core/Sources/Steps/ProfileSettingStep.swift
@@ -0,0 +1,14 @@
+import RxFlow
+
+public enum ProfileSettingStep: Step {
+    case tabsIsRequired
+    case profileSettingIsRequired(name: String, gcn: Int, email: String, password: String, isMan: Bool)
+    case privacyIsRequired(
+        name: String,
+        gcn: Int,
+        email: String,
+        password: String,
+        isMan: Bool,
+        profileImageURL: String
+    )
+}
