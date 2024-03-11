ecf0923ceade43c32c136a9f5dde263b39d0449d
juyeongno1 <juyeongno1@naver.com>
2024-03-11 22:48:49 +0900

    🧩 :: RecruitmentDetailView

--- Projects/Presentation/Sources/RecruitmentDetail/RecruitmentDetailViewModel.swift
+++ Projects/Presentation/Sources/RecruitmentDetail/RecruitmentDetailViewModel.swift
@@ -7,40 +7,60 @@ import Domain
 
 public final class RecruitmentDetailViewModel: BaseViewModel, Stepper {
     public let steps = PublishRelay<Step>()
+    public var recruitmentID: Int?
     private let disposeBag = DisposeBag()
 
     private let fetchRecruitmentDetailUseCase: FetchRecruitmentDetailUseCase
+    private let bookmarkUseCase: BookmarkUseCase
 
     init(
-        fetchRecruitmentDetailUseCase: FetchRecruitmentDetailUseCase
+        fetchRecruitmentDetailUseCase: FetchRecruitmentDetailUseCase,
+        bookmarkUseCase: BookmarkUseCase
     ) {
         self.fetchRecruitmentDetailUseCase = fetchRecruitmentDetailUseCase
+        self.bookmarkUseCase = bookmarkUseCase
     }
 
     public struct Input {
         let viewAppear: PublishRelay<Void>
         let companyDetailButtonDidClicked: Signal<Void>
+        let bookMarkButtonDidTap: Signal<Void>
     }
 
     public struct Output {
-        let recruitmentDetailInfo: PublishRelay<RecruitmentDetailEntity>
+        let recruitmentDetailEntity: PublishRelay<RecruitmentDetailEntity>
+        let areaListEntity: BehaviorRelay<[AreaEntity]>
     }
 
     public func transform(_ input: Input) -> Output {
-        let recruitmentDetailInfo = PublishRelay<RecruitmentDetailEntity>()
+        let recruitmentDetailEntity = PublishRelay<RecruitmentDetailEntity>()
+        let areaListEntity = BehaviorRelay<[AreaEntity]>(value: [])
 
-//        input.viewAppear.asObservable()
-//            .flatMap {
-//                self.fetchRecruitmentDetailUseCase.execute(id: 0)
-//            }
-//            .bind(to: recruitmentDetailInfo)
-//            .disposed(by: disposeBag)
+        input.viewAppear.asObservable()
+            .flatMap {
+                self.fetchRecruitmentDetailUseCase.execute(id: self.recruitmentID ?? 0)
+            }
+            .bind {
+                recruitmentDetailEntity.accept($0)
+                areaListEntity.accept($0.areas)
+            }
+            .disposed(by: disposeBag)
 
         input.companyDetailButtonDidClicked.asObservable()
             .map { _ in RecruitmentDetailStep.companyDetailIsRequired }
             .bind(to: steps)
             .disposed(by: disposeBag)
 
-        return Output(recruitmentDetailInfo: recruitmentDetailInfo)
+        input.bookMarkButtonDidTap.asObservable()
+            .flatMap {
+                self.bookmarkUseCase.execute(id: self.recruitmentID ?? 0)
+            }
+            .subscribe()
+            .disposed(by: disposeBag)
+
+        return Output(
+            recruitmentDetailEntity: recruitmentDetailEntity,
+            areaListEntity: areaListEntity
+        )
     }
 }
