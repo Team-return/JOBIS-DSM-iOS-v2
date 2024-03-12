bf046f4a6f5f1acd31f64e4c046a10ba81dac5c0
juyeongno1 <juyeongno1@naver.com>
2024-03-12 10:25:52 +0900

    🧩 :: baseSetting 추가

--- Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/ProjectEnvironment.swift
+++ Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/ProjectEnvironment.swift
@@ -17,5 +17,5 @@ public let env = ProjectEnvironment(
     organizationName: "com.team.return",
     deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone, .ipad]),
     platform: .iOS,
-    baseSetting: [:]
+    baseSetting: ["OTHER_LDFLAGS": .string("$(inherited)")]
 )
