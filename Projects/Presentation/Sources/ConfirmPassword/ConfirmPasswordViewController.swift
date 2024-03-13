b6f1b816179d53eaf020f2fb0e9ac6031e063fbc
hsj._.06 <gtw030488@gmail.com>
2024-03-13 11:47:01 +0900

    🧩 :: inset이구나 ㅎㅎ

--- Projects/Presentation/Sources/ConfirmPassword/ConfirmPasswordViewController.swift
+++ Projects/Presentation/Sources/ConfirmPassword/ConfirmPasswordViewController.swift
@@ -46,7 +46,7 @@ public final class ConfirmPasswordViewController: BaseViewController<ConfirmPass
         }
 
         nextButton.snp.makeConstraints {
-            $0.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(12)
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(12)
             $0.leading.trailing.equalToSuperview().inset(24)
         }
     }
