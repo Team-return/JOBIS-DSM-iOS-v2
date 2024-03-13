c0b6df0f3cd2454f6c6af34abcb6adc63617fb64
ray3238 <jjmh209@gmail.com>
2024-03-14 08:24:58 +0900

    🛠️ :: DTO 수정

--- Projects/Data/Sources/DTO/Revies/ReviewListResponseDTO.swift
+++ Projects/Data/Sources/DTO/Revies/ReviewListResponseDTO.swift
@@ -8,18 +8,20 @@ struct ReviewResponseDTO: Decodable {
     let reviewID: Int
     let year: Int
     let writer: String
+    let date: String
 
     enum CodingKeys: String, CodingKey {
         case reviewID = "review_id"
         case year
         case writer
+        case date
     }
 }
 
 extension ReviewListResponseDTO {
     func toDomain() -> [ReviewEntity] {
         reviews.map {
-            ReviewEntity(reviewID: $0.reviewID, year: $0.year, writer: $0.writer)
+            ReviewEntity(reviewID: $0.reviewID, year: $0.year, writer: $0.writer, date: $0.date)
         }
     }
 }
