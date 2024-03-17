8d78b4d4cc92824ab4e6023ee4f99dd66045c8e9
ray3238 <jjmh209@gmail.com>
2024-03-17 23:44:49 +0900

    🧩 :: NoticeDetailFlow 설정

---
+++ Projects/Flow/Sources/MyPage/Notice/NoticeDetailFlow.swift
@@ -0,0 +1,43 @@
+import UIKit
+import Presentation
+import Swinject
+import RxFlow
+import Core
+
+public final class NoticeDetailFlow: Flow {
+    public let container: Container
+    private let rootViewController: NoticeDetailViewController
+    public var root: Presentable {
+        return rootViewController
+    }
+
+    public init(container: Container) {
+        self.container = container
+        self.rootViewController = container.resolve(NoticeDetailViewController.self)!
+    }
+
+    public func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? NoticeDetailStep else { return .none }
+
+        switch step {
+        case .noticeDetailIsRequired:
+            return navigateToNoticeDetail()
+        case .noticeListIsRequired:
+            return dismissNoticeList()
+        }
+    }
+}
+
+private extension NoticeDetailFlow {
+    func navigateToNoticeDetail() -> FlowContributors {
+        return .one(flowContributor: .contribute(
+            withNextPresentable: rootViewController,
+            withNextStepper: rootViewController.viewModel
+        ))
+    }
+
+    func dismissNoticeList() -> FlowContributors {
+        rootViewController.navigationController?.popViewController(animated: true)
+        return .none
+    }
+}
