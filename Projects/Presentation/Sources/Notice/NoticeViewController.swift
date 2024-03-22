0cf360024e2e95c166134a800a11ea2eebceff73
ray3238 <jjmh209@gmail.com>
2024-03-22 20:11:52 +0900

    🛠️ :: 엔터 처리

--- Projects/Presentation/Sources/Notice/NoticeViewController.swift
+++ Projects/Presentation/Sources/Notice/NoticeViewController.swift
@@ -44,11 +44,12 @@ public final class NoticeViewController: BaseViewController<NoticeViewModel> {
                 to: noticeTableView.rx.items(
                     cellIdentifier: NoticeTableViewCell.identifier,
                     cellType: NoticeTableViewCell.self
-                )) { _, element, cell in
-                    cell.adapt(model: element)
-                    self.noticeId = element.companyId
-                }
-                .disposed(by: disposeBag)
+                ))
+        { _, element, cell in
+            cell.adapt(model: element)
+            self.noticeId = element.companyId
+        }
+        .disposed(by: disposeBag)
 
         noticeTableView.rx.modelSelected(NoticeEntity.self)
             .subscribe(onNext: { data in
