a79878e4c303176eb6fda951da432b48eab186bc
juyeongno1 <juyeongno1@naver.com>
2024-03-26 21:59:28 +0900

    🧩 :: error시 toast 띄우기

--- Projects/Core/Sources/Steps/ApplyStep.swift
+++ Projects/Core/Sources/Steps/ApplyStep.swift
@@ -4,4 +4,5 @@ public enum ApplyStep: Step {
     case applyIsRequired(recruitmentId: Int, name: String, imageURL: String)
     case reApplyIsRequired(applicationId: Int, name: String, imageURL: String)
     case popToRecruitmentDetail
+    case errorToast(message: String)
 }
