0f9589b9c26ff18996874d1c7ac18cbf9ef1f87e
juyeongno1 <juyeongno1@naver.com>
2024-03-29 08:32:40 +0900

    🧩 :: NotificationsAPI

---
+++ Projects/Data/Sources/DataSource/API/NotificationsAPI.swift
@@ -0,0 +1,59 @@
+import Moya
+import AppNetwork
+import Domain
+
+public enum NotificationsAPI {
+    case fetchNotificationList
+    case patchReadNotification(id: Int)
+}
+
+extension NotificationsAPI: JobisAPI {
+    public typealias ErrorType = JobisError
+
+    public var domain: JobisDomain {
+        return .notifications
+    }
+
+    public var urlPath: String {
+        switch self {
+        case .fetchNotificationList:
+            return ""
+
+        case let .patchReadNotification(id):
+            return "/\(id)"
+        }
+    }
+
+    public var method: Method {
+        switch self {
+        case .fetchNotificationList:
+            return .get
+
+        case .patchReadNotification:
+            return .patch
+        }
+    }
+
+    public var task: Task {
+        switch self {
+        case .fetchNotificationList:
+            return .requestParameters(
+                parameters:
+                    ["is_new": ""],
+                encoding: URLEncoding.queryString)
+        default:
+            return .requestPlain
+        }
+    }
+
+    public var jwtTokenType: JwtTokenType {
+        switch self {
+        default:
+            return .accessToken
+        }
+    }
+
+    public var errorMap: [Int: ErrorType]? {
+        return [:]
+    }
+}
