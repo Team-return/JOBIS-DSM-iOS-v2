6820fb01caaeceef61568199846eeb8ae1b2c518
HongSJae <gtw030488@gmail.com>
2024-03-28 14:57:02 +0900

    🛠️ :: Dependency Refactor

--- Projects/Flow/Project.swift
+++ Projects/Flow/Project.swift
@@ -6,10 +6,8 @@ let project = Project.makeModule(
     name: "Flow",
     product: .staticLibrary,
     targets: [.unitTest],
-    packages: [.FCM],
     dependencies: [
         .Projects.data,
-        .Projects.presentation,
-        .SPM.FCM
+        .Projects.presentation
     ]
 )
