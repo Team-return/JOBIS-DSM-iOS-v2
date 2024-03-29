67d23cee0bd40bf74ca53860ed739c82aa99c89d
HongSJae <gtw030488@gmail.com>
2024-03-29 21:27:44 +0900

    🧩 :: CI update

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,6 +3,10 @@ cd ../
 git clone https://github.com/Team-return/JOBIS-v2-XCConfig.git
 mv JOBIS-v2-XCConfig/XCConfig/ .
 
+cd ../Projects/App/Resources
+git clone https://github.com/Team-return/JOBIS-GoogleInfo.git
+mv JOBIS-GoogleInfo/FireBase/ .
+
 brew install make
 curl -Ls https://install.tuist.io | bash
 
