9f94eb9feaab5672b58dd85bec18afa664f55d89
hsj._.06 <gtw030488@gmail.com>
2024-03-29 20:55:21 +0900

    🧩 :: Template Google Info 처리

--- Tuist/ProjectDescriptionHelpers/Action+Template.swift
+++ Tuist/ProjectDescriptionHelpers/Action+Template.swift
@@ -6,4 +6,21 @@ public extension TargetScript {
         name: "SwiftLint",
         basedOnDependencyAnalysis: false
     )
+    
+    static let googleInfoPlistScripts = TargetScript.pre(
+        script: """
+                case "${CONFIGURATION}" in
+                    "DEV" )
+                        cp -r "$SRCROOT/Resources/Firebase/GoogleService-Dev-Info.plist" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/GoogleService-Info.plist" ;;
+                    "STAGE" )
+                        cp -r "$SRCROOT/Resources/Firebase/GoogleService-Stage-Info.plist" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/GoogleService-Info.plist" ;;
+                    "PROD" )
+                        cp -r "$SRCROOT/Resources/Firebase/GoogleService-Prod-Info.plist" "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.app/GoogleService-Info.plist" ;;
+                    *)
+                    ;;
+                esac
+                """,
+        name: "GoogleService-Info.plist",
+        basedOnDependencyAnalysis: false
+    )
 }
