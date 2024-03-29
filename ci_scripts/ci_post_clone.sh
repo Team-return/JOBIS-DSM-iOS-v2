151f2abfa4b396d9eb1e2d90030233add1c5aadd
hsj._.06 <gtw030488@gmail.com>
2024-03-29 11:30:27 +0900

    🧩 :: CI update

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-cd ..
-
 cd ../
 git clone https://github.com/Team-return/JOBIS-v2-XCConfig.git
 mv JOBIS-v2-XCConfig/XCConfig/ .
@@ -9,4 +6,5 @@ mv JOBIS-v2-XCConfig/XCConfig/ .
 brew install make
 curl -Ls https://install.tuist.io | bash
 
-make ci_generate
+tuist fetch
+TUIST_CI=1 tuist generate
