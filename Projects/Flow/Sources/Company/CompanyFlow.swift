16775df53bd2badd715a6a16a483f386570387fd
ray3238 <jjmh209@gmail.com>
2024-04-02 08:37:32 +0900

    🧩 :: CompanyFlow 설정

--- Projects/Flow/Sources/Company/CompanyFlow.swift
+++ Projects/Flow/Sources/Company/CompanyFlow.swift
@@ -24,6 +24,8 @@ public final class CompanyFlow: Flow {
             return navigateToCompany()
         case let .companyDetailIsRequired(id):
             return navigateToCompanyDetail(id)
+        case .companySearchIsRequired:
+            return navigateToCompanySearch()
         }
     }
 }
@@ -51,6 +53,22 @@ private extension CompanyFlow {
         return .one(flowContributor: .contribute(
             withNextPresentable: companyDetailFlow,
             withNextStepper: OneStepper(withSingleStep: CompanyDetailStep.companyDetailIsRequired)
+        ))
+    }
+
+    func navigateToCompanySearch() -> FlowContributors {
+        let companySearchFlow = CompanySearchFlow(container: container)
+
+        Flows.use(companySearchFlow, when: .created) { (root) in
+            let view = root as? CompanySearchViewController
+            self.rootViewController.navigationController?.pushViewController(
+                view!, animated: true
+            )
+        }
+
+        return .one(flowContributor: .contribute(
+            withNextPresentable: companySearchFlow,
+            withNextStepper: OneStepper(withSingleStep: CompanySearchStep.companySearchIsRequired)
         ))
     }
 }
