4a12ed2a3aa1b9a3626000f743a9cc0c3426e62a
juyeongno1 <juyeongno1@naver.com>
2024-08-19 18:42:26 +0900

    🛠️ :: ci_scripts 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,7 +7,14 @@ git clone https://github.com/Team-return/JOBIS-GoogleInfo.git
 mv JOBIS-GoogleInfo/FireBase/ Projects/App/Resources/
 
 brew install make
-curl -Ls https://install.tuist.io | bash
+
+curl https://mise.jdx.dev/install.sh | sh
+export PATH="$HOME/.local/bin:$PATH"
+eval "$(mise activate bash --shims)"
+
+mise install tuist@3.23.1
+
+tuist version
 
 tuist fetch
 TUIST_CI=1 tuist generate
