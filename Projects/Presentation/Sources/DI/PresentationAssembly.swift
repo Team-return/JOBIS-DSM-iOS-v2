1535947076b8ded58e34e37e356c06ed05559c5c
Choyoungjun <cyj070513@gmail.com>
2024-10-16 11:55:06 +0900

    🧩 :: 겨울인턴 추가

--- Projects/Presentation/Sources/DI/PresentationAssembly.swift
+++ Projects/Presentation/Sources/DI/PresentationAssembly.swift
@@ -14,7 +14,8 @@ public final class PresentationAssembly: Assembly {
             HomeViewModel(
                 fetchStudentInfoUseCase: resolver.resolve(FetchStudentInfoUseCase.self)!,
                 fetchApplicationUseCase: resolver.resolve(FetchApplicationUseCase.self)!,
-                fetchBannerListUseCase: resolver.resolve(FetchBannerListUseCase.self)!
+                fetchBannerListUseCase: resolver.resolve(FetchBannerListUseCase.self)!,
+                fetchWinterInternUseCase: resolver.resolve(FetchWinterInternSeasonUseCase.self)!
             )
         }
 
