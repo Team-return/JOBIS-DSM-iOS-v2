5ccb28796e44a213ee429dcabce5190c673fa7aa
circle0802 <hawon9781@gmail.com>
2025-02-12 23:33:51 +0900

    🧩 :: charview publishing 완료 및 색 추가

--- Projects/Presentation/Sources/Home/EmployStatus/Components/ChartView.swift
+++ Projects/Presentation/Sources/Home/EmployStatus/Components/ChartView.swift
@@ -8,48 +8,101 @@ import DGCharts
 
 final class ChartView: BaseView {
     private let disposeBag = DisposeBag()
+    private let chartContainerView = UIView()
     private var employPieChartView = PieChartView().then {
         $0.rotationEnabled = false
         $0.highlightPerTapEnabled = false
         $0.holeRadiusPercent = 0.6
         $0.legend.enabled = false
         let entries = [
-            PieChartDataEntry(value: 30, label: ""),
-            PieChartDataEntry(value: 70, label: "")
+            PieChartDataEntry(value: 0, label: ""),
+            PieChartDataEntry(value: 0, label: "")
         ]
 
         let dataSet = PieChartDataSet(entries: entries)
         dataSet.colors = [.Sub.skyBlue10, .Primary.blue20]
         dataSet.drawValuesEnabled = false
+        dataSet.selectionShift = 0
 
         let data = PieChartData(dataSet: dataSet)
         $0.data = data
     }
     private let employPercentageLabel = UILabel().then {
-        $0.setJobisText("70%", font: .subHeadLine, color: .Primary.blue20)
+        $0.setJobisText("\(0)%", font: .boldBody, color: .Primary.blue20)
+    }
+    private let employLabel = UILabel().then {
+        $0.setJobisText("취업 현황", font: .subBody, color: .Sub.skyBlue30)
+    }
+    private let stickView = UIView().then {
+        $0.backgroundColor = .GrayScale.gray40
+        $0.layer.cornerRadius = 7
+    }
+    private let totalStatsLabel = UILabel().then {
+        $0.setJobisText("전체 통계", font: .description, color: .Sub.skyBlue30)
+    }
+    private let totalStatsValueLabel = UILabel().then {
+        $0.setJobisText("\(0)/\(0)명", font: .description, color: .Primary.blue20)
     }
     private let legendView = UIStackView().then {
         $0.axis = .vertical
         $0.spacing = 4
         $0.alignment = .leading
     }
-
     override func addView() {
         [
-            employPieChartView,
-            legendView
+            chartContainerView,
+            employLabel,
+            legendView,
+            totalStatsLabel,
+            totalStatsValueLabel
         ].forEach { self.addSubview($0) }
+        [
+            employPieChartView,
+            stickView,
+            totalStatsLabel,
+            totalStatsValueLabel
+        ].forEach { chartContainerView.addSubview($0) }
         employPieChartView.addSubview(employPercentageLabel)
     }
     override func setLayout() {
+        chartContainerView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.equalTo(120)
+            $0.width.equalTo(215)
+        }
+
         employPieChartView.snp.makeConstraints {
             $0.centerY.equalToSuperview()
-            $0.leading.equalToSuperview().inset(5)
-            $0.height.width.equalTo(160)
+            $0.leading.equalToSuperview()
+            $0.height.width.equalTo(120)
         }
+
+        stickView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(employPieChartView.snp.trailing).offset(16)
+            $0.height.equalTo(30)
+            $0.width.equalTo(1)
+        }
+
+        totalStatsLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(38)
+            $0.centerX.equalTo(totalStatsValueLabel)
+        }
+
+        totalStatsValueLabel.snp.makeConstraints {
+            $0.top.equalTo(totalStatsLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(stickView.snp.trailing).offset(21)
+        }
+
         employPercentageLabel.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
+
+        employLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20)
+            $0.leading.equalToSuperview().inset(16)
+        }
+
         legendView.snp.makeConstraints {
             $0.top.equalToSuperview().inset(20)
             $0.trailing.equalToSuperview().inset(16)
@@ -57,7 +110,7 @@ final class ChartView: BaseView {
     }
     override func configureView() {
         super.configureView()
-        backgroundColor = .white
+        backgroundColor = .GrayScale.gray10
         layer.cornerRadius = 6
         layer.borderWidth = 1
         layer.borderColor = UIColor.GrayScale.gray30.cgColor
