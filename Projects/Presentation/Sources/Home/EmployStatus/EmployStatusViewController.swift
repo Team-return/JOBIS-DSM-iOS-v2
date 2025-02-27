40d75b586ee2a3ac477bb26dd4ffc0c33369d44e
circle0802 <hawon9781@gmail.com>
2025-02-27 16:56:15 +0900

    🧩 :: EmployStatusViewController ScrollView 및 classbutton layout

--- Projects/Presentation/Sources/Home/EmployStatus/EmployStatusViewController.swift
+++ Projects/Presentation/Sources/Home/EmployStatus/EmployStatusViewController.swift
@@ -9,27 +9,76 @@ import DesignSystem
 import Charts
 
 public final class EmployStatusViewController: BaseViewController<EmployStatusViewModel> {
+    private let scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+    }
+    private let contentView = UIView()
     private let chartView = ChartView()
     private let classEmploymentLabel = UILabel().then {
         $0.setJobisText("반별 취업 현황 확인하기", font: .largeBody, color: .GrayScale.gray60)
     }
+    private let classButton1 = ClassButton(iconLabel: "💻", classNumber: 1)
+    private let classButton2 = ClassButton(iconLabel: "💻", classNumber: 2)
+    private let classButton3 = ClassButton(iconLabel: "🔧", classNumber: 3)
+    private let classButton4 = ClassButton(iconLabel: "🤖", classNumber: 4)
+    private lazy var classRow1 = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 16
+        $0.distribution = .fillEqually
+    }
+
+    private lazy var classRow2 = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 16
+        $0.distribution = .fillEqually
+    }
 
     public override func addView() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
         [
             chartView,
-            classEmploymentLabel
-        ].forEach { view.addSubview($0) }
+            classEmploymentLabel,
+            classRow1,
+            classRow2
+        ].forEach { contentView.addSubview($0) }
+        [
+            classButton1,
+            classButton2
+        ].forEach { classRow1.addArrangedSubview($0) }
+        [
+            classButton3,
+            classButton4
+        ].forEach { classRow2.addArrangedSubview($0) }
     }
 
     public override func setLayout() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalTo(self.view.safeAreaLayoutGuide)
+        }
+
+        contentView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView.contentLayoutGuide)
+            $0.width.equalTo(scrollView.frameLayoutGuide)
+        }
+
         chartView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(12)
+            $0.top.equalToSuperview().inset(12)
             $0.leading.trailing.equalToSuperview().inset(24)
             $0.height.equalTo(264)
         }
         classEmploymentLabel.snp.makeConstraints {
             $0.top.equalTo(chartView.snp.bottom).offset(20)
             $0.leading.trailing.equalToSuperview().inset(24)
+        }
+        classRow1.snp.makeConstraints {
+            $0.top.equalTo(classEmploymentLabel.snp.bottom).offset(7)
+            $0.centerX.equalToSuperview()
+        }
+        classRow2.snp.makeConstraints {
+            $0.top.equalTo(classRow1.snp.bottom).offset(16)
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.bottom.equalToSuperview().offset(-20)
         }
     }
 
