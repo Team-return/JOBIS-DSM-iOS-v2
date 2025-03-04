4cdd154554e7951bd47fb803821e726ba273568d
jihoon <leejh080628@naver.com>
2025-03-04 14:21:08 +0900

    🧩 :: 각 반 enum 처리

--- Projects/Presentation/Sources/Home/EmployStatus/ClassEmploymentViewController.swift
+++ Projects/Presentation/Sources/Home/EmployStatus/ClassEmploymentViewController.swift
@@ -98,13 +98,7 @@ public final class ClassEmploymentViewController: BaseViewController<ClassEmploy
     }
 
     private func getClassTitle(for classNumber: Int) -> String {
-        switch classNumber {
-        case 1: return "소프트웨어 개발 1반"
-        case 2: return "소프트웨어 개발 2반"
-        case 3: return "임베디드 개발과"
-        case 4: return "인공지능 개발과"
-        default: return "\(classNumber)반"
-        }
+        return ClassTitle(rawValue: classNumber)?.title ?? "\(classNumber)반"
     }
 
     public override func configureNavigation() {
