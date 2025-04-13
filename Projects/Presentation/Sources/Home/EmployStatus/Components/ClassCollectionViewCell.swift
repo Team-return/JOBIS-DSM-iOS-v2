cb1adb338c765a3a45d61a88f7ce8ab5c2d7a458
circle0802 <hawon9781@gmail.com>
2025-04-13 22:45:09 +0900

    🎨 :: ClassCollectionViewCell background dark 색 변경

--- Projects/Presentation/Sources/Home/EmployStatus/Components/ClassCollectionViewCell.swift
+++ Projects/Presentation/Sources/Home/EmployStatus/Components/ClassCollectionViewCell.swift
@@ -9,7 +9,7 @@ final class ClassCollectionViewCell: BaseCollectionViewCell<EmploymentCompany> {
     static let identifier = "ClassCollectionViewCell"
     private let backView = UIView().then {
         $0.layer.cornerRadius = 6
-        $0.backgroundColor = .GrayScale.gray10
+        $0.backgroundColor = .GrayScale.gray0
         $0.layer.shadowColor = UIColor(red: 112/255, green: 144/255, blue: 176/255, alpha: 0.12).cgColor
         $0.layer.shadowOpacity = 1
         $0.layer.shadowRadius = 16
