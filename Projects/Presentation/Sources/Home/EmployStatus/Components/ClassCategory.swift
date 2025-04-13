6eb80657d07418bfbcecef47057a69642298349d
circle0802 <hawon9781@gmail.com>
2025-04-13 23:31:08 +0900

    🎨 :: ClassCategory 이름 변경

--- Projects/Presentation/Sources/Home/EmployStatus/Components/ClassCategory.swift
+++ Projects/Presentation/Sources/Home/EmployStatus/Components/ClassCategory.swift
@@ -10,8 +10,8 @@ enum ClassCategory: Int {
         switch self {
         case .softwareDev1: return "소프트웨어 개발 1반"
         case .softwareDev2: return "소프트웨어 개발 2반"
-        case .embedded: return "임베디드 개발과"
-        case .ai: return "인공지능 개발과"
+        case .embedded: return "임베디드 소프트웨어 3반"
+        case .ai: return "인공지능 소프트웨어 4반"
         }
     }
 }
