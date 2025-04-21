d5796049174dfe57805900517a91ddcec39bdcda
jihoon <leejh080628@naver.com>
2025-04-21 19:03:38 +0900

    🧩 :: 관심분야 flow 추가

---
+++ Projects/Flow/Sources/MyPage/InterestField/InterestFieldFlow.swift
@@ -0,0 +1,36 @@
+import UIKit
+import Presentation
+import Swinject
+import RxFlow
+import Core
+
+public final class InterestFieldFlow: Flow {
+    public let container: Container
+    private let rootViewController: InterestFieldViewController
+    public var root: Presentable {
+        return rootViewController
+    }
+    
+    public init(container: Container) {
+        self.container = container
+        self.rootViewController = container.resolve(InterestFieldViewController.self)!
+    }
+    
+    public func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? InterestFieldStep else { return .none }
+        
+        switch step {
+        case .interestFieldIsRequired:
+            return navigateToInterestField()
+        }
+    }
+}
+
+    private extension InterestFieldFlow {
+        func navigateToInterestField() -> FlowContributors {
+            return .one(flowContributor: .contribute(
+                withNextPresentable: rootViewController,
+                withNextStepper: rootViewController.viewModel
+            ))
+        }
+    }
