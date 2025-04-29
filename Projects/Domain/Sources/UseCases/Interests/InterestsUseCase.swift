c0d824b313a83ab9beef50b34ca758133b40ba05
jihoon <leejh080628@naver.com>
2025-04-29 20:14:47 +0900

    🧩 :: lint 수정

--- Projects/Domain/Sources/UseCases/Interests/InterestsUseCase.swift
+++ Projects/Domain/Sources/UseCases/Interests/InterestsUseCase.swift
@@ -2,11 +2,11 @@ import RxSwift
 
 public struct InterestsUseCase {
     private let interestsRepository: any InterestsRepository
-    
+
     public init(interestsRepository: any InterestsRepository) {
         self.interestsRepository = interestsRepository
     }
-    
+
     public func execute() -> Single<[InterestsEntity]> {
         interestsRepository.fetchInterests()
     }
