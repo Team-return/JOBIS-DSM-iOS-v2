19f4985a48a2955b112e25c766e46c21953f9176
jihoon <leejh080628@naver.com>
2025-05-01 20:09:56 +0900

    🧩 :: 자잘한 수정

--- Projects/Data/Sources/Repositories/InterestsRepositoryImpl.swift
+++ Projects/Data/Sources/Repositories/InterestsRepositoryImpl.swift
@@ -3,11 +3,11 @@ import Domain
 
 struct InterestsRepositoryImpl: InterestsRepository {
     private let remoteInterestsDataSource: any RemoteInterestsDataSource
-    
+
     init(remoteInterestsDataSource: any RemoteInterestsDataSource) {
         self.remoteInterestsDataSource = remoteInterestsDataSource
     }
-    
+
     func fetchInterests() -> Single<[InterestsEntity]> {
         remoteInterestsDataSource.fetchInterests()
     }
