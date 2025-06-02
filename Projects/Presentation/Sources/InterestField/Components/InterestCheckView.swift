070377ff4856317595d4946b1b21838cdb2bdbb7
jihoon <leejh080628@naver.com>
2025-06-02 12:09:22 +0900

    🧩 :: 관심분야 선택 완료 뷰 레이아웃 수정

--- Projects/Presentation/Sources/InterestField/Components/InterestCheckView.swift
+++ Projects/Presentation/Sources/InterestField/Components/InterestCheckView.swift
@@ -9,19 +9,22 @@ final class InterestCheckView: BaseView {
 
     private let checkLabel = UILabel().then {
         $0.setJobisText(
-            "님의/n 관심사를 확인했어요",
+            "님의 \n관심사를 확인했어요!",
             font: .smallBody,
             color: .GrayScale.gray90
         )
         $0.numberOfLines = 0
+        $0.textAlignment = .center
     }
 
     private let checkExplainLabel = UILabel().then {
         $0.setJobisText(
-            "관심사에 맞는 모집 의뢰서가 업로드되면 알림을 드립니다!",
+            "관심사에 맞는 모집 의뢰서가 업로드되면\n알림을 드립니다!",
             font: .description,
             color: .GrayScale.gray80
         )
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
     }
 
     override func addView() {
@@ -34,7 +37,20 @@ final class InterestCheckView: BaseView {
 
     override func setLayout() {
         checkImageView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(80)
+        }
+
+        checkLabel.snp.makeConstraints {
+            $0.top.equalTo(checkImageView.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(24)
+        }
+
+        checkExplainLabel.snp.makeConstraints {
+            $0.top.equalTo(checkLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview().inset(24)
         }
     }
+
 }
