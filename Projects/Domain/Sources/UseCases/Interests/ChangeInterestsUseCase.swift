7b2721847f188a990c28bc93ffddaed1467d652c
jihoon <leejh080628@naver.com>
2025-06-12 20:09:29 +0900

    🧩 :: 관심분야 변경 UseCase 수정

--- Projects/Domain/Sources/UseCases/Interests/ChangeInterestsUseCase.swift
+++ Projects/Domain/Sources/UseCases/Interests/ChangeInterestsUseCase.swift
@@ -8,6 +8,10 @@ public struct ChangeInterestsUseCase {
     }
 
     public func execute(interests: [InterestsEntity]) -> Completable {
-        interestsRepository.updateInterests(interestsIDs: interests.map { $0.interestID })
+        interestsRepository.updateInterests(interestsIDs: interests.map { $0.code })
+    }
+
+    public func execute(codeIDs: [Int]) -> Completable {
+        interestsRepository.updateInterests(interestsIDs: codeIDs)
     }
 }
