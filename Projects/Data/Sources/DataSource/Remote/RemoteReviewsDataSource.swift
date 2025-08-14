6d3d9e098f3ea5c8d2f37d63ffb1a6cffdd40362
jihoon <leejh080628@naver.com>
2025-08-14 14:13:54 +0900

    🗂 :: RemoteReviewsDataSource 코드 스타일 통일

--- Projects/Data/Sources/DataSource/Remote/RemoteReviewsDataSource.swift
+++ Projects/Data/Sources/DataSource/Remote/RemoteReviewsDataSource.swift
@@ -1,13 +1,19 @@
 import RxSwift
 import Domain
 
-protocol RemoteReviewsDataSource {
+public protocol RemoteReviewsDataSource {
     func postReview(req: PostReviewRequestQuery) -> Completable
+    func fetchReviewListPageCount(req: ReviewListPageCountRequestQuery) -> Single<ReviewListPageCountResponseDTO>
 }
 
 final class RemoteReviewsDataSourceImpl: RemoteBaseDataSource<ReviewsAPI>, RemoteReviewsDataSource {
     func postReview(req: PostReviewRequestQuery) -> Completable {
         request(.postReview(req))
             .asCompletable()
+    }
+    
+    func fetchReviewListPageCount(req: ReviewListPageCountRequestQuery) -> Single<ReviewListPageCountResponseDTO> {
+        request(.reviewListPageCount(req))
+            .map(ReviewListPageCountResponseDTO.self)
     }
 }
