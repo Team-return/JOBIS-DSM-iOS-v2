862f50e2d3e667722f4d37ed5c8dfd26066a46da
circle0802 <hawon9781@gmail.com>
2025-08-27 16:40:31 +0900

    ✨ :: (#323) 필터, 검색 버튼 추가

--- Projects/Presentation/Sources/Review/ReviewViewController.swift
+++ Projects/Presentation/Sources/Review/ReviewViewController.swift
@@ -8,7 +8,19 @@ import Core
 import DesignSystem
 
 public final class ReviewViewController: BaseViewController<ReviewViewModel> {
+    private let filterButton = UIButton().then {
+        $0.setImage(.jobisIcon(.filterIcon), for: .normal)
+    }
+    private let searchButton = UIButton().then {
+        $0.setImage(.jobisIcon(.searchIcon), for: .normal)
+    }
+
     public override func configureNavigation() {
         self.setLargeTitle(title: "후기")
+
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(customView: searchButton),
+            UIBarButtonItem(customView: filterButton)
+        ]
     }
 }
