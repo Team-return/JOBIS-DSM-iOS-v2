ddecb6c11372214cec646175c2a89ea78629b13c
jihoon <leejh080628@naver.com>
2025-11-25 22:28:31 +0900

    ✨ :: (#348) Reactor 추가

---
+++ Projects/Presentation/Sources/Notice/NoticeDetailReactor.swift
@@ -0,0 +1,51 @@
+import ReactorKit
+import RxSwift
+import RxCocoa
+import RxFlow
+import Core
+import Domain
+
+public final class NoticeDetailReactor: BaseReactor, Stepper {
+    public let steps = PublishRelay<Step>()
+    public let initialState: State
+    private let disposeBag = DisposeBag()
+    private let fetchNoticeDetailUseCase: FetchNoticeDetailUseCase
+    public var noticeID: Int?
+
+    init(fetchNoticeDetailUseCase: FetchNoticeDetailUseCase) {
+        self.initialState = .init()
+        self.fetchNoticeDetailUseCase = fetchNoticeDetailUseCase
+    }
+
+    public enum Action {
+        case viewDidAppear
+    }
+
+    public enum Mutation {
+        case setNoticeDetail(NoticeDetailEntity)
+    }
+
+    public struct State {
+        var noticeDetail: NoticeDetailEntity?
+    }
+}
+
+extension NoticeDetailReactor {
+    public func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .viewDidAppear:
+            return fetchNoticeDetailUseCase.execute(id: noticeID ?? 0)
+                .asObservable()
+                .map { Mutation.setNoticeDetail($0) }
+        }
+    }
+
+    public func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .setNoticeDetail(detail):
+            newState.noticeDetail = detail
+        }
+        return newState
+    }
+}
---
+++ Projects/Presentation/Sources/Notice/NoticeReactor.swift
@@ -0,0 +1,58 @@
+import ReactorKit
+import RxSwift
+import RxCocoa
+import RxFlow
+import Core
+import Domain
+
+public final class NoticeReactor: BaseReactor, Stepper {
+    public let steps = PublishRelay<Step>()
+    public let initialState: State
+    private let disposeBag = DisposeBag()
+    private let fetchNoticeListUseCase: FetchNoticeListUseCase
+
+    init(fetchNoticeListUseCase: FetchNoticeListUseCase) {
+        self.initialState = .init()
+        self.fetchNoticeListUseCase = fetchNoticeListUseCase
+    }
+
+    public enum Action {
+        case viewDidAppear
+        case noticeDidSelect(Int)
+    }
+
+    public enum Mutation {
+        case setNoticeList([NoticeEntity])
+        case navigateToDetail(Int)
+    }
+
+    public struct State {
+        var noticeList: [NoticeEntity] = []
+    }
+}
+
+extension NoticeReactor {
+    public func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .viewDidAppear:
+            return fetchNoticeListUseCase.execute()
+                .asObservable()
+                .map { Mutation.setNoticeList($0) }
+
+        case let .noticeDidSelect(id):
+            return .just(.navigateToDetail(id))
+        }
+    }
+
+    public func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .setNoticeList(list):
+            newState.noticeList = list
+
+        case let .navigateToDetail(id):
+            steps.accept(NoticeStep.noticeDetailIsRequired(id: id))
+        }
+        return newState
+    }
+}
