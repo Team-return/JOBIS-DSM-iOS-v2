e4eda67ca9586a330b0bbcb0846764e6d32fee8a
Juhui <joohee090331@gmail.com>
2026-02-17 03:04:35 +0900

    ✨ :: 기업탐색 정렬 추가

--- Projects/Presentation/Sources/Company/CompanyViewController.swift
+++ Projects/Presentation/Sources/Company/CompanyViewController.swift
@@ -23,6 +23,8 @@ public final class CompanyViewController: BaseReactorViewController<CompanyReact
         $0.setImage(.jobisIcon(.searchIcon), for: .normal)
     }
 
+    private let jobisDropdownView = JobisDropdownView(options: ["기본순", "매출", "직원 ↓", "직원 ↑", "설립일 ↓", "설립일 ↑"])
+
     public override func addView() {
         [
             companyTableView
@@ -83,6 +85,10 @@ public final class CompanyViewController: BaseReactorViewController<CompanyReact
 
     public override func configureViewController() {
         companyTableView.dataSource = self
+        companyTableView.tableHeaderView = jobisDropdownView
+        jobisDropdownView.frame = CGRect(x: 0, y: 0, width: companyTableView.bounds.width, height: 28)
+        companyTableView.tableHeaderView = jobisDropdownView
+
 
         viewWillAppearPublisher.asObservable()
             .bind {
