36b60abc24fbe395f2af4af6f87c1217ac90c2e0
Juhui <joohee090331@gmail.com>
2026-02-18 02:56:16 +0900

    🗂 :: JobisDropdownCell 파일 분리

--- Projects/Modules/DesignSystem/Sources/Dropdown/JobisDropdownCell.swift
+++ Projects/Modules/DesignSystem/Sources/Dropdown/JobisDropdownCell.swift
@@ -1,8 +1,25 @@
-//
-//  JobisDropdownCell.swift
-//  DesignSystem
-//
-//  Created by 신주희 on 2/15/26.
-//  Copyright © 2026 com.team.return. All rights reserved.
-//
+import UIKit
 
+class JobisDropdownCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupCell() {
+        textLabel?.font = .jobisFont(.description)
+        textLabel?.textAlignment = .center
+        selectionStyle = .gray
+        backgroundColor = .white
+    }
+
+    func configure(with text: String, isSelected: Bool) {
+        textLabel?.text = text
+        textLabel?.textColor = isSelected ? .Primary.blue20 : .GrayScale.gray60
+    }
+}
--- Projects/Modules/DesignSystem/Sources/Dropdown/JobisDropdownView.swift
+++ Projects/Modules/DesignSystem/Sources/Dropdown/JobisDropdownView.swift
@@ -145,27 +145,3 @@ extension JobisDropdownView: UITableViewDelegate, UITableViewDataSource {
         tableView.reloadData()
     }
 }
-
-class JobisDropdownCell: UITableViewCell {
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupCell()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    private func setupCell() {
-        textLabel?.font = .jobisFont(.description)
-        textLabel?.textAlignment = .center
-        selectionStyle = .gray
-        backgroundColor = .white
-    }
-
-    func configure(with text: String, isSelected: Bool) {
-        textLabel?.text = text
-        textLabel?.textColor = isSelected ? .Primary.blue20 : .GrayScale.gray60
-    }
-}
