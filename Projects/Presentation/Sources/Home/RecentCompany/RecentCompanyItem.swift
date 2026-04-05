00308b441981e92d736aa2cb3186050c3b3100bc
Juhui <joohee090331@gmail.com>
2026-04-05 17:52:31 +0900

    ✨ :: (#416) 최근 본 기업 Item 생성

---
+++ Projects/Presentation/Sources/Home/RecentCompany/RecentCompanyItem.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Domain
+
+import DesignSystem
+
+public struct RecentCompanyItem {
+    public let entity: RecentCompanyEntity
+    
+    public init(entity: RecentCompanyEntity) {
+        self.entity = entity
+    }
+}
